4ca0d72890a7b6aa19744c9d45722becb9e2d928
lxereadout <lxe.readout@gmail.com>
2015-02-12 11:34:56 -0800

    updated capacitance plot
    defined named variables

--- capacitance.py
+++ capacitance.py
@@ -2,19 +2,36 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 
-LXeH = np.arange(0.0004, 0.0246, 0.00004)
+eps = 1e-5
+anode_cathode_distance = 0.0248286 # m
+LXeH = np.arange(eps, anode_cathode_distance-eps, 0.00004)
 cpct = []
 
-for i in LXeH:
-    c1 = 1.96*8.854*(10**-12)*0.0248286/i
-    c2 = 1*8.854*(10**-12)*0.0248286/(0.02464-i)
+for z in LXeH:
+    lxe_dielectric_constant = 1.96
+    gxe_dielectric_constant = 1.0
+    vacuum_permittivity = 8.854e-12
+    
+    
+    c1 = lxe_dielectric_constant*vacuum_permittivity*anode_cathode_distance/z
+    c2 = gxe_dielectric_constant*vacuum_permittivity*anode_cathode_distance/(anode_cathode_distance-z)
+    
+    # total capacitance (add in series, convert to pF)
     cpct.append(10**12*(1/(1/c1 + 1/c2)))
+    
+print "capacitance with no LXe [pF]", cpct[0]
+print "capacitance full of LXe [pF]", cpct[-1]
 
 plt.plot(LXeH, cpct)
+plt.grid(b=True)
 plt.title('Capacitance vs. Height')
-plt.xlabel('LXe height in m')
+plt.xlabel('LXe height from cathode in m')
 plt.ylabel('Capacitance in pF')
-plt.show()
+plt.savefig("capacitance.jpg")
+print "printed"
+plt.clf()
+    
+# stuff below here is not used    
 
 def LXe_height(volume):
     if volume > 0.00126:
