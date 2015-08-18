b0bb5259c976e9e8441a35183e1a807bfe6cdabc
alexis-s <alexis4@stanford.edu>
2015-08-18 16:38:19 -0700

    added correlation plots for different channels

--- struck/drawSisEvents.py
+++ struck/drawSisEvents.py
@@ -53,36 +53,49 @@ def process_file(filename):
     canvas.SetGrid(1,1)
 
     # set up parameters for histograms
-    n_bins = 200
+    n_bins = 100
     min_bin = 0
-    max_bin = 2500
+    max_bin = 2000
+
 
     # draw coherent sum hist
     hist0 = TH1D("hist0", "", n_bins, min_bin, max_bin)
     hist0.SetLineWidth(2)
     hist0.SetLineColor(TColor.kBlue)
     tree.Draw("totalEnergy >> hist0","","goff")
+    #tree.Draw("energy[1]+energy[2] >> hist0","energy[1]+energy[2]>0","goff")
+    print "entries in coherent hist0:", hist0.GetEntries()
 
     # draw incoherent sum hist
     hist1 = TH1D("hist1", "", n_bins, min_bin, max_bin)
     hist1.SetLineWidth(2)
     hist1.SetLineColor(TColor.kRed)
-    tree.Draw("energy >> hist1","","goff")
+    tree.Draw("energy >> hist1","energy>0","goff")
+    #tree.Draw("energy[1] >> hist1","energy[1]>0","goff")
+    #print "entries in hist1:", hist1.GetEntries()
+    #tree.Draw("energy[2] >> +hist1","energy[2]>0","goff")
+    print "entries in incoherent hist1:", hist1.GetEntries()
 
     # single-wire hist
     hist2 = TH1D("hist2", "", n_bins, min_bin, max_bin)
     hist2.SetLineWidth(2)
     hist2.SetLineColor(TColor.kGreen+1)
     tree.Draw("totalEnergy >> hist2","nHits==1","goff")
+    #tree.Draw("energy[1] >> hist2","energy[2]==0 && energy[1]>0","goff")
+    #print "entries in hist2:", hist2.GetEntries()
+    #tree.Draw("energy[2] >> +hist2","energy[1]==0 && energy[2]>0","goff")
+    print "entries in single-wire hist2:", hist2.GetEntries()
 
     # multi-wire hist
     hist3 = TH1D("hist3", "", n_bins, min_bin, max_bin)
     hist3.SetLineWidth(2)
     hist3.SetLineColor(TColor.kBlack)
     tree.Draw("totalEnergy >> hist3","nHits>1","goff")
+    #tree.Draw("energy[1]+energy[2] >> hist3","energy[1]>0 && energy[2]>0","goff")
+    print "entries in multi-wire hist3:", hist3.GetEntries()
 
     # set up a legend
-    legend = TLegend(0.1, 0.91, 0.9, 0.99)
+    legend = TLegend(0.11, 0.91, 0.9, 0.99)
     legend.AddEntry(hist0, "total energy", "l")
     legend.AddEntry(hist1, "wire energies", "l")
     legend.AddEntry(hist2, "total energy, n wires == 1", "l")
@@ -96,9 +109,100 @@ def process_file(filename):
     hist1.Draw("same")
     hist2.Draw("same")
     hist3.Draw("same")
+    canvas.SetLeftMargin(0.11)
+    hist0.GetYaxis().SetTitleOffset(1.2)
     legend.Draw()
     canvas.Update()
     canvas.Print("%s_event_spectrum.png" % basename)
+    canvas.SetLogy(0)
+    hist0.SetMaximum(1800)
+    canvas.Update()
+    canvas.Print("%s_event_spectrum_lin.png" % basename)
+
+
+    #-------------------------------------------------------------------------------
+    # repeat for different channel pair combinations
+    #-------------------------------------------------------------------------------
+
+    # I have just been modifying these options by hand and running the script
+    # multple times...
+    #pair = [0, 1] # modify this line only
+    #pair = [0, 2] # modify this line only
+    pair = [1, 2] # modify this line only
+
+
+    # these are the channel assignments
+    title = ["X29", "X28", "X27"]
+    # [0] = X29
+    # [0] = X28
+    # [0] = X27
+
+    # make titles for plot naming
+    ch1_title = title[pair[0]]
+    ch2_title = title[pair[1]]
+
+    # make strings to use with TTree:Draw()
+    ch1 = "energy[%i]" % pair[0]
+    ch2 = "energy[%i]" % pair[1]
+
+    print "drawing %s vs %s: %s and %s" % (ch1_title, ch2_title, ch1, ch2)
+
+
+    # draw coherent sum hist
+    #tree.Draw("totalEnergy >> hist0","","goff")
+    tree.Draw("%s+%s >> hist0" % (ch1, ch2),"%s+%s>0" % (ch1, ch2),"goff")
+    print "entries in coherent hist0:", hist0.GetEntries()
+
+    # draw incoherent sum hist
+    #tree.Draw("energy >> hist1","","goff")
+    tree.Draw("%s >> hist1" % ch1,"%s>0" % ch1,"goff")
+    print "entries in hist1:", hist1.GetEntries()
+    tree.Draw("%s >> +hist1" % ch2,"%s>0" % ch2,"goff")
+    print "entries in incoherent hist1:", hist1.GetEntries()
+
+    # single-wire hist
+    #tree.Draw("totalEnergy >> hist2","nHits==1","goff")
+    tree.Draw("%s >> hist2" % ch1,"%s==0 && %s>0" % (ch2, ch1),"goff")
+    print "entries in hist2:", hist2.GetEntries()
+    tree.Draw("%s >> +hist2" % ch2,"%s==0 && %s>0" % (ch1, ch2),"goff")
+    print "entries in single-wire hist2:", hist2.GetEntries()
+
+    # multi-wire hist
+    #tree.Draw("totalEnergy >> hist3","nHits>1","goff")
+    tree.Draw("%s+%s >> hist3" % (ch1, ch2),"%s>0 && %s>0" % (ch1, ch2),"goff")
+    print "entries in multi-wire hist3:", hist3.GetEntries()
+
+    # set up a legend
+    legend = TLegend(0.11, 0.91, 0.9, 0.99)
+    legend.AddEntry(hist0, "total energy", "l")
+    legend.AddEntry(hist1, "wire energies", "l")
+    legend.AddEntry(hist2, "total energy, n wires == 1", "l")
+    legend.AddEntry(hist3, "total energy, n wires > 1", "l")
+    legend.SetNColumns(2)
+
+    hist0.SetXTitle("Energy [keV]")
+    hist0.SetYTitle("Counts / %i keV" % (max_bin/n_bins))
+
+    hist0.Draw()
+    hist1.Draw("same")
+    hist2.Draw("same")
+    hist3.Draw("same")
+    canvas.SetLeftMargin(0.11)
+    hist0.GetYaxis().SetTitleOffset(1.2)
+    legend.Draw()
+    canvas.SetLogy(1)
+    hist0.SetMaximum(-1111)
+    canvas.Update()
+    canvas.Print("%s_event_spectrum_%s_%s.png" % (basename, ch1_title, ch2_title))
+    canvas.SetLogy(0)
+    hist0.SetMaximum(1500)
+    canvas.Update()
+    canvas.Print("%s_event_spectrum_lin_%s_%s.png" % (basename, ch1_title, ch2_title))
+
+
+    #-------------------------------------------------------------------------------
+    # draw some 2D correlation plots
+    #-------------------------------------------------------------------------------
 
     canvas.SetLogy(0)
     canvas.SetLeftMargin(0.11)
