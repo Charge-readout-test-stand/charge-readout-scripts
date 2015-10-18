2ff3192ec21004353c060475a5af9ad8a68fc60b
Alexis Schubert <alexis4@stanford.edu>
2015-10-18 15:23:24 -0700

    added ability to write root file if called from command line

--- mca/convertMCA.py
+++ mca/convertMCA.py
@@ -1,6 +1,12 @@
+#!/bin/env python
+
+from ROOT import gROOT
+gROOT.SetBatch(True)
 import ROOT
 import numpy as np
-import matplotlib.pyplot as plt
+import sys
+import os.path
+from array import array
 
 ROOT.gROOT.SetStyle("Plain")
 ROOT.gStyle.SetOptStat(0)
@@ -11,7 +17,7 @@ def getMCAhist(file_name):
     data = np.genfromtxt(file_name,skip_header=12, skip_footer=44)
     
     #bin 1 is first and bin nbins is last
-    hist = ROOT.TH1D(file_name, file_name, len(data), 0, len(data))
+    hist = ROOT.TH1D("hist", file_name, len(data), 0, len(data))
 
     livetime_line = ""
     file_check = open(file_name)
@@ -37,5 +43,53 @@ def getMCAhist(file_name):
         i+=1
 
     return hist, livetime
+
+
+
+
+if __name__ == "__main__":
+
+
+    """
+    If this script is called from the command line, save a root file
+    """
+    
+    # grab filenames from arguments:
+    filenames = sys.argv[1:]
+
+    # if no arguments are provided, print some usage info
+    if len(filenames) < 1:
+        print "arguments: [mca files]"
+        sys.exit(1)
+
+    # loop over all provided MCA files, call  getMCAhist(), and save root output:
+    for filename in filenames:
+
+        basename = os.path.basename(filename) # remove path
+        print "--> processing", basename
+        basename = os.path.splitext(basename)[0] # remove file extension
+
+        outfile = ROOT.TFile("%s.root" % basename, "recreate")
+        livetime = array('d', [0]) # double
+        tree = ROOT.TTree("tree","livetime data tree")
+        tree.Branch('livetime', livetime, 'livetime/D')
+
+        hist, livetime[0] = getMCAhist(filename)
+
+        print "\t livetime: %.2f seconds" % livetime[0]
+
+        print "\t %i entries in hist" % hist.GetEntries()
+        hist.SetLineColor(ROOT.TColor.kBlue+1)
+        hist.SetFillColor(ROOT.TColor.kBlue+1)
+        hist.SetFillStyle(3004)
+        hist.Write()
+
+        tree.Fill()
+        tree.Write()
+
+        outfile.Close()
+
+
+
 
 
