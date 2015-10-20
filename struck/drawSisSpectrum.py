3be949f66323d091213721aa76bb7a129a8044b1
alexis-s <alexis4@stanford.edu>
2015-10-20 13:12:56 -0700

    now using baseline values to make spectrum

--- struck/drawSisSpectrum.py
+++ struck/drawSisSpectrum.py
@@ -6,7 +6,7 @@ assumed to exist:
 * max
 * channel
 
-arguments [sis root files]
+arguments [sis tier 1 root files]
 """
 
 import os
@@ -33,6 +33,45 @@ gStyle.SetTitleStyle(0)
 gStyle.SetTitleBorderSize(0)       
 
 
+def draw_hist(basename, tree, channel, color=TColor.kBlack, fill_style=3004):
+
+    # set up parameters for histograms
+    min_bin = 0
+    max_bin = pow(2, 14) # ADC max is 2^14
+    n_bins = max_bin
+
+    # find baseline
+    canvas = TCanvas("canvas1","")
+    canvas.SetLogy(1)
+    max_bin = pow(2, 14) # ADC max is 2^14
+    baseline_hist = TH1D("baseline_hist", "ch %i baseline" % channel, max_bin, 0, max_bin)
+    n_entries = tree.Draw("wfm >> baseline_hist","channel == %i && Iteration$<350" % channel)
+    baseline_hist.SetLineColor(TColor.kBlue+1)
+    baseline_hist.SetFillColor(TColor.kBlue+1)
+    mean = baseline_hist.GetMean()
+
+    if n_entries > 0:
+        print "%i entries in baseline hist" % n_entries
+        baseline_hist.SetAxisRange(mean-200, mean+200)
+        print "mean", mean
+        canvas.Update()
+        canvas.Print("%s_ch%i_baseline.png" % (basename, channel))
+    
+
+    name = "hist%i" % channel
+    hist = TH1D(name, "", n_bins, min_bin, max_bin)
+    hist.SetLineWidth(2)
+    hist.SetLineColor(color)
+    hist.SetFillColor(color)
+    hist.SetFillStyle(fill_style)
+    n_entries = tree.Draw("adc_max - %s >> %s" % (mean, name), "channel==%i" % channel, "goff")
+    if n_entries > 0:
+        print "entries in ch %i:" % channel, n_entries
+    max_bin_height = hist.GetBinContent(hist.GetMaximumBin())
+
+    return hist, max_bin_height
+
+
 def process_file(filename):
 
     print "processing file: ", filename
@@ -48,6 +87,7 @@ def process_file(filename):
     canvas = TCanvas("canvas","")
     canvas.SetLogy(1)
     canvas.SetGrid(1,1)
+    canvas.SetTopMargin(0.2)
 
     # find the maximum energy value in the tree, among all channels
     print "total entries in tree", tree.GetEntries()
@@ -56,66 +96,44 @@ def process_file(filename):
     print "max digitized value", max_mca_value
     print "min digitized value", min_mca_value
 
-    # set up parameters for histograms
-    #n_bins = 200
-    #min_bin = min_mca_value - 1
-    #max_bin = max_mca_value + 1
-    min_bin = 0
-    max_bin = pow(2, 14) # ADC max is 2^14
-    n_bins = max_bin/64
-    print "n_bins", n_bins
+    max_bin_height = 0
 
-    # draw channel 0
-    hist0 = TH1D("hist0", "", n_bins, min_bin, max_bin)
-    hist0.SetLineWidth(2)
-    hist0.SetLineColor(TColor.kBlue)
-    hist0.SetFillColor(TColor.kBlue)
-    hist0.SetFillStyle(3004)
-    print "entries in ch0:", tree.Draw("adc_max >> hist0","channel==0","goff")
-    max_bin_height0 = hist0.GetBinContent(hist0.GetMaximumBin())
+    colors = [TColor.kBlue, TColor.kRed, TColor.kGreen+1, TColor.kViolet]
+    fillStyle = [3004, 3005, 3006, 3007, 3001, 3002, 3003, 3016,
+      3017, 3018, 3020, 3021] 
 
-    # draw channel 1
-    hist1 = TH1D("hist1", "", n_bins, min_bin, max_bin)
-    hist1.SetLineWidth(2)
-    hist1.SetLineColor(TColor.kRed)
-    hist1.SetFillColor(TColor.kRed)
-    hist1.SetFillStyle(3005)
-    print "entries in ch1:", tree.Draw("adc_max >> hist1","channel==1","goff")
-    max_bin_height1 = hist1.GetBinContent(hist1.GetMaximumBin())
-
-    # draw channel 2
-    hist2 = TH1D("hist2", "", n_bins, min_bin, max_bin)
-    hist2.SetLineWidth(2)
-    hist2.SetLineColor(TColor.kGreen+1)
-    hist2.SetFillColor(TColor.kGreen+1)
-    hist2.SetFillStyle(3006)
-    print "entries in ch2:", tree.Draw("adc_max >> hist2","channel==2","goff")
-    max_bin_height2 = hist2.GetBinContent(hist2.GetMaximumBin())
-
-    # find the highest max, among all 3 channels:
-    max = max_bin_height0
-    if (max_bin_height1 > max): max = max_bin_height1
-    if (max_bin_height2 > max): max = max_bin_height2
-
-    hist0.SetMaximum(max*1.1)
-    hist0.SetXTitle("Energy [ADC units]")
-    hist0.SetYTitle("Counts")
+    hists = []
 
     # set up a legend
-    legend = TLegend(0.1, 0.91, 0.9, 0.99)
-    legend.AddEntry(hist0, "ch 0", "f")
-    legend.AddEntry(hist1, "ch 1", "f")
-    legend.AddEntry(hist2, "ch 2", "f")
-    legend.SetNColumns(3)
+    legend = TLegend(0.1, 0.81, 0.9, 0.99)
+    legend.SetNColumns(4)
 
-    hist0.Draw()
-    hist1.Draw("same")
-    hist2.Draw("same")
+    for channel in xrange(16):
+
+        i_color = channel % len(colors)
+        i_fill = channel % len(fillStyle)
+        hist, i_max_bin_height = draw_hist(basename, tree, channel, colors[i_color],
+        fillStyle[i_fill])
+        n_entries = hist.GetEntries()
+
+        legend.AddEntry(hist, "ch %i (%i)" % (channel, n_entries), "f")
+        hists.append(hist)
+
+        if i_max_bin_height > max_bin_height:
+            max_bin_height = i_max_bin_height
+
+    hists[0].SetAxisRange(0, 200)
+    hists[0].SetMaximum(max_bin_height*1.1)
+    hists[0].SetXTitle("Energy [ADC units]")
+    hists[0].SetYTitle("Counts")
+    hists[0].Draw()
+
+
+    for hist in hists[1:]:
+        hist.Draw("same")
     legend.Draw()
     canvas.Update()
     canvas.Print("%s_spectrum.png" % basename)
-
-
 
 
 
