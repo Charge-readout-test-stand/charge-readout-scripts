532d2cb272cab9f6ac7e4225529f0296edf60cb2
Alexis Schubert <alexis4@stanford.edu>
2015-12-04 09:20:57 -0800

    updates for 6th LXe

--- struck/struck_analysis_parameters.py
+++ struck/struck_analysis_parameters.py
@@ -7,6 +7,9 @@ import this into your script with this line:
 import struck_analysis_parameters
 
 This file must be in the same directory as your script. 
+
+notes:
+* ortec preamp added 04 Dec for 6th LXe 
 """
 
 
@@ -20,22 +23,31 @@ except ImportError:
     print "couldn't import CLHEP/ROOT"
     microsecond = 1.0e3
 
-
+is_6th_LXe = True
 
 
 sampling_freq_Hz = 25.0e6 # digitizer sampling frequency, Hz
 
-# channels for 5th LXe
-channels = [0,1,2,3,4,8]
-pmt_channel = 8
 
-# channel names for 5th LXe    
+# in software, struck channels start from 0, not 1
+pmt_channel = 8
+if is_6th_LXe:
+    # channels for 6th LXe
+    channels = [0,1,2,3,4,5,8]
+else:
+    # channels for 5th LXe
+    channels = [0,1,2,3,4,8]
+
+
+# channel names for 6th LXe    
 channel_map = {}
 channel_map[0] = "X26"
 channel_map[1] = "X27"
 channel_map[2] = "X29"
 channel_map[3] = "Y23"
 channel_map[4] = "Y24"
+if is_6th_LXe:
+    channel_map[5] = "X2" # ortec preamp added 04 Dec for 6th LXe
 channel_map[8] = "PMT"
 
 
@@ -48,8 +60,12 @@ decay_time_values[2] = 775.0*microsecond
 decay_time_values[3] = 750.0*microsecond
 decay_time_values[4] = 450.0*microsecond
 
+if is_6th_LXe:
+    # Ortec 142-IH manual says 200 to 300 microseconds... we should measure this.
+    decay_time_values[5] = 300.0*microsecond
 
-# charge calbration from these files:
+
+# charge calbration from these files for 5th LXe:
 # tier3_LXe_Run1_1700VC_2chargechannels_609PM_60thresh_NotShaped_Amplified_GapTime20_2_0.root
 # tier3_LXe_Run1_1700VC_5chargechannels_315PM_5thresh_0.root
 # tier3_LXe_Run1_1700VC_5chargechannels_315PM_8thresh_0.root
@@ -63,6 +79,10 @@ calibration_values[1] = 1.0/1.84579440737210035e+02*570.0/0.6
 calibration_values[2] = 1.0/1.90907907272149885e+02*570.0/0.56
 calibration_values[3] = 1.0/2.94300492610837466e+02*570.0/0.38
 calibration_values[4] = 1.0/1.40734817170111285e+02*570.0/0.725
+
+if is_6th_LXe:
+    # a guess
+    calibration_values[5] = 1.0/1.40734817170111285e+02*570.0/0.725
 
 # PMT calibration is from PMT-triggered data
 calibration_values[8] = 570.0/2550.0*2.0
@@ -95,6 +115,15 @@ def is_amplified(baseline_mean_file, baseline_rms_file):
 
 if __name__ == "__main__":
     gROOT.SetBatch(True)
+
+
+    print "\nchannels used:"
+    for channel in channels:
+        print "\t channel %i" % channel
+
+    print "\nchannel names:"
+    for (channel, name) in channel_map.items():
+        print "\t channel %i: %s" % (channel, name)
 
     print "\nlinear calibration info:"
     for (channel, value) in calibration_values.items():
