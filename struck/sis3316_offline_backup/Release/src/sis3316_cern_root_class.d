27a6b21144f629e58fc0d02c0d476df4037dda77
phsunrise <phsun@stanford.edu>
2015-12-10 14:10:20 -0800

    added sis3316 backup

---
+++ struck/sis3316_offline_backup/Release/src/sis3316_cern_root_class.d
@@ -0,0 +1,322 @@
+src/sis3316_cern_root_class.d src/sis3316_cern_root_class.o: \
+ ../src/sis3316_cern_root_class.cpp ../src/sis3316_cern_root_class.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TApplication.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TObject.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/Rtypes.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/RConfig.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/RVersion.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/DllImport.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/Rtypeinfo.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/snprintf.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/strlcpy.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TGenericClassInfo.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TSchemaHelper.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TStorage.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TVersionCheck.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/Riosfwd.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TBuffer.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TQObject.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TString.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TMathBase.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TApplicationImp.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TGLabel.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TGFrame.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TGWindow.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TGObject.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/GuiTypes.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TGClient.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TVirtualX.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TNamed.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TAttLine.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TAttFill.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TAttText.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TAttMarker.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TGDimension.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TGGC.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TRefCnt.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TGFont.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TGLayout.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TGString.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TList.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TSeqCollection.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TCollection.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TIterator.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TObject.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TH1.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TAxis.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TAttAxis.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TArrayD.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TArray.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TArrayC.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TArrayS.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TArrayI.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TArrayF.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/Foption.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TVectorFfwd.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TVectorDfwd.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TFitResultPtr.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TH1D.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TH1F.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TH2D.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TH2.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TMatrixFBasefwd.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TMatrixDBasefwd.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TGraph.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TGraph2D.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TVirtualHistPainter.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TMultiGraph.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TF1.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TFormula.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TBits.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TObjArray.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/Math/ParamFunctor.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TMath.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TError.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TCanvas.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TPad.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TVirtualPad.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TAttPad.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/Buttons.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TAttBBox2D.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TAttCanvas.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TCanvasImp.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TSystem.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TInetAddress.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TTimer.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TSysEvtHandler.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TTime.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/ThreadLocalStorage.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/RConfigure.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TLatex.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TText.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TGFrame.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TPaveLabel.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TPave.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TBox.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TFrame.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TWbox.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TColor.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TGNumberEntry.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TGTextEntry.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TGWidget.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/WidgetMessageTypes.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TGTextBuffer.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TGButton.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TRootEmbeddedCanvas.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TGCanvas.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TGProgressBar.h \
+ /nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TStyle.h
+
+../src/sis3316_cern_root_class.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TApplication.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TObject.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/Rtypes.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/RConfig.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/RVersion.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/DllImport.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/Rtypeinfo.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/snprintf.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/strlcpy.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TGenericClassInfo.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TSchemaHelper.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TStorage.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TVersionCheck.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/Riosfwd.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TBuffer.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TQObject.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TString.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TMathBase.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TApplicationImp.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TGLabel.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TGFrame.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TGWindow.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TGObject.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/GuiTypes.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TGClient.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TVirtualX.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TNamed.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TAttLine.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TAttFill.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TAttText.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TAttMarker.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TGDimension.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TGGC.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TRefCnt.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TGFont.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TGLayout.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TGString.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TList.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TSeqCollection.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TCollection.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TIterator.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TObject.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TH1.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TAxis.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TAttAxis.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TArrayD.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TArray.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TArrayC.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TArrayS.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TArrayI.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TArrayF.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/Foption.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TVectorFfwd.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TVectorDfwd.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TFitResultPtr.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TH1D.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TH1F.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TH2D.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TH2.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TMatrixFBasefwd.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TMatrixDBasefwd.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TGraph.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TGraph2D.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TVirtualHistPainter.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TMultiGraph.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TF1.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TFormula.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TBits.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TObjArray.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/Math/ParamFunctor.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TMath.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TError.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TCanvas.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TPad.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TVirtualPad.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TAttPad.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/Buttons.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TAttBBox2D.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TAttCanvas.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TCanvasImp.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TSystem.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TInetAddress.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TTimer.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TSysEvtHandler.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TTime.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/ThreadLocalStorage.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/RConfigure.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TLatex.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TText.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TGFrame.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TPaveLabel.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TPave.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TBox.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TFrame.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TWbox.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TColor.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TGNumberEntry.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TGTextEntry.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TGWidget.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/WidgetMessageTypes.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TGTextBuffer.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TGButton.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TRootEmbeddedCanvas.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TGCanvas.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TGProgressBar.h:
+
+/nfs/slac/g/exo/software/ROOT/rhel6/root/5.34.21/include/TStyle.h:
