535137751a71986e48f990c8e2d6b8bd44430705
Alexis Schubert <alexis4@stanford.edu>
2015-12-11 17:21:30 -0800

    removing .o and .d files

--- rootGUI/Release/src/get_configuration_parameters.d
+++
@@ -1,7 +0,0 @@
-src/get_configuration_parameters.d: \
-  ../src/get_configuration_parameters.cpp ../src/project_system_define.h \
-  ../src/get_configuration_parameters.h
-
-../src/project_system_define.h:
-
-../src/get_configuration_parameters.h:
