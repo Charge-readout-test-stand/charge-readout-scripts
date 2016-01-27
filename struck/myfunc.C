141e74eb2437afc86efa6228f3775795886dfc05
Alexis Schubert <alexis4@stanford.edu>
2016-01-27 09:48:20 -0800

    wip

--- struck/myfunc.C
+++ struck/myfunc.C
@@ -12,7 +12,8 @@ Double_t OnePadRotated(Double_t x, Double_t y, Double_t z) {
   // response from one pad. Coordinates are rotated 45 degrees into pad's
   // coordinate system. 
   Double_t diagonal = 3.0; // mm
-  Double_t side_length = diagonal / TMath::Sqrt(2.0);
+  // subtract 10 microns to avoid double counting:
+  Double_t side_length = diagonal / TMath::Sqrt(2.0); // - 0.01;
 
   // calculate new variables in rotated coord system of anode pad:
   Double_t x_n = (x - y)/TMath::Sqrt(2);
@@ -27,10 +28,10 @@ Double_t OnePadRotated(Double_t x, Double_t y, Double_t z) {
   // handle things if z goes to zero:
   if ( z <= 0) {
       // if the charge is outside the pad, return 0
-      if ( x_n < x1 ) return 0;
-      if ( x_n > x2 ) return 0;
-      if ( y_n < y1 ) return 0;
-      if ( x_n > y2 ) return 0;
+      if ( x_n <= -side_length/2.0 ) { return 0; }
+      if ( x_n > side_length/2.0 ) { return 0; } 
+      if ( y_n <= -side_length/2.0 ) { return 0; } 
+      if ( y_n > side_length/2.0 ) { return 0; } 
       // otherwise return 1
       return 1.0;
   }
@@ -38,8 +39,20 @@ Double_t OnePadRotated(Double_t x, Double_t y, Double_t z) {
   return ( f(x2,y2,z) - f(x1,y2,z) - f(x2,y1,z) + f(x1,y1,z) ) / TMath::TwoPi();
 }
 
+Double_t OneStrip(Double_t x, Double_t y, Double_t z) {
+  // return amplitude from one strip:
+  Double_t diagonal = 3.0; // mm
+  Double_t amplitude = 0.0;
+  for ( int i_pad = 0; i_pad <= 30; i_pad++ ){
+      amplitude += OnePadRotated(x + i_pad*diagonal - diagonal*14.5, y, z);
+      //cout << i_pad << ": " << amplitude << endl;
+  }
+  return amplitude;
+}
+
 Double_t MyFunction(Double_t *var, Double_t *par) {
-  // reponse of the strip (currently just one pad)
+  // reponse of the strip (30 pads in x direction)
+  // FIXME -- add cathode effect
 
   Double_t triggerTime = 8.0; // microseconds
   Double_t driftVelocity = 1.72; // mm/microsecond
@@ -49,22 +62,18 @@ Double_t MyFunction(Double_t *var, Double_t *par) {
   Double_t z0 = par[2];
   Double_t q = par[3]; 
   Double_t z = z0 - (t-triggerTime) * driftVelocity;  
-  cout << "t: " << t << endl;
+  //cout << "t: " << t << endl;
   if (t < triggerTime) { return 0.0; }
-  if (z <= 0) { 
-      return 1.0; 
-  }
-  cout << "z: " << z << endl;
+  //cout << "z: " << z << endl;
 
-  Double_t amplitude = 0.0;
-  amplitude += OnePadRotated(x, y, z);
-  cout << "amplitude: " << amplitude << endl;
+  Double_t amplitude = OneStrip(x,y,z) - OneStrip(x,y,z0);
+  //cout << "amplitude: " << amplitude << endl;
   return q*amplitude; 
 }
 
 Double_t myfunc() {
     // a test -- just draw a sample wfm to see if things are working. 
-
+    
     TF1* test = new TF1("test",MyFunction, 6, 20, 4);
     test->SetParameter(0, 0); // x
     test->SetParameter(1, 0); // y
