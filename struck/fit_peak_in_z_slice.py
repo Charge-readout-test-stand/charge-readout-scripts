7f311d6a482dfbec83dd000103121334c3935c86
Alexis Schubert <alexis4@stanford.edu>
2016-04-05 20:32:02 -0700

    testing 3 or 4 slices of detector

--- struck/fit_peak_in_z_slice.py
+++ struck/fit_peak_in_z_slice.py
@@ -21,11 +21,15 @@ def process_file(filename):
     # options 
     all_energy_var = "energy1_pz"
     max_drift_length = 19.0 # mm
-    n_slices = 19
+    n_slices = 19 # 1-mm slices
+    #n_slices = 3 # 3 detector regions
+    #n_slices = 4 # 3 detector regions
     single_strip_cut = struck_analysis_parameters.get_single_strip_cut(10.0)
     n_bins = 160
     min_bin = 0
     max_bin = 1600
+    fit_half_width=300
+    #fit_half_width=170
 
     selections = []
     selections.append(single_strip_cut)
@@ -57,23 +61,51 @@ def process_file(filename):
     legend.SetNColumns(5)
 
     hists = []
+    out_file = TFile("%s_%i.root" % (basename, n_slices), "recreate")
 
     hist = TH1D("hist_all", "",n_bins,min_bin,max_bin)
     tree.Draw("%s >> %s" % (all_energy_var, hist.GetName()),"","goff")
+    print "%i entries in hist %s" % (hist.GetEntries(), hist.GetName())
     hists.append(hist)
     legend.AddEntry(hist,"all","l")
 
     hist = TH1D("hist_ss", "",n_bins,min_bin,max_bin)
     tree.Draw("%s >> %s" % (all_energy_var, hist.GetName()),single_strip_cut,"goff")
+    print "%i entries in hist %s" % (hist.GetEntries(), hist.GetName())
     hists.append(hist)
     legend.AddEntry(hist,"single-strip","l")
 
     t = 0.0
     i = 0
+    if n_slices == 4:
+        legend.SetNColumns(3)
     while t < max_drift_length/drift_velocity: # microseconds
+
+        # special handling for 3 slices: close to anode, flat region, cathode
+        if n_slices == 3:
+            if i == 0:
+                dt = struck_analysis_parameters.drift_time_threshold
+            elif i == 1:
+                t = dt
+                dt = 18.0/drift_velocity-t
+            elif i == 2:
+                dt = 1.0/drift_velocity
+
+        if n_slices == 4:
+            if i == 0:
+                dt=4.0
+            elif i == 1:
+                t = 4.0
+                dt = struck_analysis_parameters.drift_time_threshold-t
+            elif i == 2:
+                t = struck_analysis_parameters.drift_time_threshold
+                dt = 18.0/drift_velocity-t
+            elif i == 3:
+                dt = 1.0/drift_velocity
+            
         
-        print "---> %i: t=%.1f microseconds" % (i,t)
-        drift_time_cut = "(rise_time_stop95-trigger_time>%s)&&(rise_time_stop95-trigger_time<%s)" % (t, t+dt)
+        print "---> %i: t=%.2f microseconds, dt=%.2f" % (i,t, dt)
+        drift_time_cut = "(rise_time_stop99-trigger_time>%s)&&(rise_time_stop99-trigger_time<%s)" % (t, t+dt)
         selection = " && ".join(selections + [drift_time_cut])
         print selection
 
@@ -87,9 +119,9 @@ def process_file(filename):
             all_energy_var=all_energy_var, 
             selection=selection,
             do_use_step=True,    
-            min_bin=200,
-            max_bin=1000,
-            fit_half_width = 300,
+            min_bin=300,
+            max_bin=1200,
+            fit_half_width=fit_half_width,
             do_use_exp=True,
         )
         result["dt"] = dt
@@ -98,8 +130,8 @@ def process_file(filename):
         hist = TH1D("hist_%i_to_%i" % (t*1e3, (t+dt)*1e3), "",n_bins,min_bin,max_bin)
         hists.append(hist)
         tree.Draw("%s >> %s" % (all_energy_var, hist.GetName()),selection,"goff")
+        print "%i entries in hist %s" % (hist.GetEntries(), hist.GetName())
         legend.AddEntry(hist,"%.1f to %.1f #mus" % (t, t+dt),"l")
-        print "%i entries in %s" % (hist.GetEntries(), hist.GetName())
         plot_name = "fit_all_%s_lin.pdf" % basename
         new_plot_name = "z_slice_fit_%i_to_%i.pdf" % (t*1e3, (t+dt)*1e3)
         cmd = "mv -f %s %s" % (plot_name, new_plot_name)
@@ -142,6 +174,8 @@ def process_file(filename):
 
     if not gROOT.IsBatch():
         raw_input("enter to continue ")
+
+    out_file.Write()
 
 
 if __name__ == "__main__":
