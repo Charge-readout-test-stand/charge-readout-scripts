8317f07df14b74ddba416c82dbf623b9bfb333c5
Alexis Schubert <alexis4@stanford.edu>
2016-04-12 16:22:21 -0700

    updated MC W-value; added 7th LXe PMT decay times; moved selection fcns to struck_analysis_cuts

--- struck/struck_analysis_parameters.py
+++ struck/struck_analysis_parameters.py
@@ -103,6 +103,7 @@ if is_7th_LXe:
     channel_map[5] = "Y17"
     channel_map[6] = "Y18"
     channel_map[7] = "Y19"
+    channel_map[9] = "PMT"
 
 
 #MC Channels index starts at 0 so X26 = 25
@@ -154,198 +155,13 @@ def is_tree_MC(tree):
     except:
         return False
 
-def get_corrected_energy_cmd(energy_var="energy1_pz"):
-    ""
-    return "energy1_pz*(1-(%s-x)*.01/(%s-%s))" % (
-        max_drift_time,
-        max_drift_time,
-        drift_time_threshold,
-    )
-
-
-def get_negative_energy_cut(threshold=-20.0):
-    """
-    return a cut on events with too much negative energy on any one channel
-    """
-
-    selection = []
-    for channel, value  in enumerate(charge_channels_to_use): 
-        if value:
-            cut = "(energy1_pz[%i]<%s)" % (
-                channel, 
-                threshold,
-            )
-            #print cut
-            selection.append(cut)
-            
-    # join each channel requirement with or
-    selection = " || ".join(selection)
-
-    # enclose in parentheses and add not
-    selection = "!(%s)" % selection
-
-    return selection
-
-
-def get_short_drift_time_cut(
-    energy_threshold=200.0,
-    drift_time_cut=drift_time_threshold,
-):
-    """
-    Cut any events with energy above threshold and too short a drift time
-    """
-
-    selection = []
-    for channel, value  in enumerate(charge_channels_to_use): 
-        if value:
-            cut = "((energy1_pz[%i]>%s) && (rise_time_stop99[%i]-trigger_time<%s))" % (
-                channel,
-                energy_threshold,
-                channel,
-                drift_time_cut,
-            )
-            #print cut
-            selection.append(cut)
-
-    # join each channel requirement with or
-    selection = " || ".join(selection)
-
-    # enclose in parentheses and add not
-    selection = "!(%s)" % selection
-
-    return selection
-
-
-
-def get_long_drift_time_cut(
-    energy_threshold=200.0,
-    drift_time_low=drift_time_threshold,
-    drift_time_high=None,
-):
-    """
-    Select events with energy above threshold and long enough drift time
-    """
-
-    selection = []
-    for channel, value  in enumerate(charge_channels_to_use): 
-        if value:
-            cut = []
-            if energy_threshold != None:
-                part = "(energy1_pz[%i]>%s)" % (
-                    channel, 
-                    energy_threshold,
-                )
-                cut.append(part)
-            if drift_time_high != None:
-                part = "(rise_time_stop99[%i]-trigger_time>%s)" % (
-                    channel, 
-                    drift_time_low,
-                )
-                cut.append(part)
-            if drift_time_high != None:
-                part = "(rise_time_stop99[%i]-trigger_time<%s)" % (
-                    channel,
-                    drift_time_high,
-                )
-                cut.append(part)
-            #print cut
-            cut = "&&".join(cut)
-            selection.append(cut)
-            
-    # join each channel requirement with or
-    selection = " || ".join(selection)
-
-    # enclose in parentheses
-    selection = "(%s)" % selection
-
-    return selection
-
-def get_chargeEnergy_no_pz():
-    """A draw command for total energy before PZ correction """
-    draw_cmd = []
-    for channel, value in enumerate(charge_channels_to_use): 
-        if value:
-            draw_cmd.append("energy1[%i]" % channel)
-    # join each part with "+"
-    draw_cmd = " + ".join(draw_cmd)
-    return draw_cmd
-
-def get_multiplicity_cmd(energy_threshold=100.0):
-    """A draw command for multiplicity """
-    draw_cmd = []
-    for channel, value in enumerate(charge_channels_to_use): 
-        if value:
-            draw_cmd.append("(energy1_pz[%i]>%s)" % (channel,energy_threshold))
-    # join each part with "+"
-    draw_cmd = "+".join(draw_cmd)
-    return draw_cmd
-
-def get_single_strip_cut(energy_threhold=10.0):
-    """Select events with only one channel above threshold"""
-    selection = "(%s==1)" % get_multiplicity_cmd(energy_threhold)
-    return selection
-
-def get_few_channels_cmd(
-    energy_threshold=100.0,
-):
-    """ A draw command for total energy, only including  events above threshold """
-    draw_cmd = []
-    for channel, value  in enumerate(charge_channels_to_use): 
-        if value:
-            part = "(energy1_pz[%i]>%s)*energy1_pz[%i]" % (
-                channel, 
-                energy_threshold,
-                channel,
-            )
-            #print part
-            draw_cmd.append(part)
-    # join each channel requirement with or
-    draw_cmd = " + ".join(draw_cmd)
-    return draw_cmd
-
-
-def get_cuts_label(draw_cmd, selection):
-
-    label = []
-
-    # check draw command
-    if get_few_channels_cmd() in draw_cmd:
-        label.append("FC")
-    if get_chargeEnergy_no_pz() in draw_cmd:
-        label.append("NPZ")
-
-    # check selection
-    if get_negative_energy_cut() in selection:
-        label.append("NC")
-    if get_short_drift_time_cut() in selection:
-        label.append("SC")
-    if get_long_drift_time_cut() in selection:
-        label.append("LC")
-    label = "+".join(label)
-    if label == "": label = "No_cuts"
-    return label
-
-def get_energy_weighted_drift_time():
-    selection = []
-
-    for channel, value  in enumerate(charge_channels_to_use): 
-        if value:
-            cut = "energy1_pz[%i]*(rise_time_stop99[%i]-trigger_time)" % (
-                channel, 
-                channel, 
-            )
-            selection.append(cut)
-    selection = " + ".join(selection)
-    selection = "(%s)/chargeEnergy" % selection
-    return selection
-
-
 
 #Wvalue for Xenon
 #From EXODimensions.hh
 #const double W_VALUE_IONIZATION = 15.6*CLHEP::eV;
 #const double W_VALUE_LXE_EV_PER_ELECTRON = 18.7*CLHEP::eV;
-Wvalue = 18.7 #eV needed to make 1e-
+# from NumTE from 1 MeV electrons: 1e3/4.96561538461538439e+04 
+Wvalue = 20.138 #eV needed to make 1e- 
 
 # values from Peihao, 31 Oct 2015:
 decay_time_values = {}
@@ -358,6 +174,7 @@ decay_time_values[4] = 450.0*microsecond
 if is_6th_LXe:
     # Ortec 142-IH manual says 200 to 300 microseconds... we should measure this.
     decay_time_values[5] = 300.0*microsecond
+    decay_time_values[pmt_channel] = 1e9*CLHEP.microsecond
 
 if is_7th_LXe:
     # updated with fits to 7th LXe overnight -- 30 Mar 2016
@@ -371,6 +188,7 @@ if is_7th_LXe:
     decay_time_values[5] = 439.96*microsecond # +/- 1.24 
     decay_time_values[6] = 417.95*microsecond # +/- 1.77 
     decay_time_values[7] = 448.86*microsecond # +/- 2.91 
+    decay_time_values[9] = 1.5*CLHEP.microsecond 
 
 
 # charge calbration from these files for 5th LXe:
@@ -555,7 +373,12 @@ if __name__ == "__main__":
     print "RMS contribution to energy1_pz, energy1_pz_rms_keV: %.2f" % energy1_pz_rms_keV 
     print "digitization noise contribution to energy1_pz, energy1_pz_digitization_noise_keV %.2f" % energy1_pz_digitization_noise_keV
     print "intrinsic resolution of 570 keV, from NEST [%]:", "%.2f" % (nest_resolution_570*100.0)
-    expected_resolution_570 = math.sqrt( (nest_resolution_570*570.0)**2 + energy1_pz_digitization_noise_keV**2 + energy1_pz_rms_keV**2 )
+    expected_resolution_570 = math.sqrt( \
+        (nest_resolution_570*570.0)**2 \
+        + energy1_pz_digitization_noise_keV**2 \
+        + energy1_pz_rms_keV**2 \
+        + (570*0.01)**2 # 1% broadening after z cut
+    )
     print "expected resolution of energy1_pz @ 570 keV [keV]: %.2f" % expected_resolution_570
     print "expected resolution of energy1_pz @ 570 keV [%]:","%.2f" % (expected_resolution_570/570.0*100.0)
 
@@ -565,32 +388,3 @@ if __name__ == "__main__":
     #    print "color %i:" % i, color
 
 
-    print "\nnegative energy cut:"
-    print "\t" + "\n\t ||".join(get_negative_energy_cut().split("||"))
-
-    print "\nshort drift time cut:"
-    print "\t" + "\n\t ||".join(get_short_drift_time_cut().split("||"))
-
-    print "\nlong drift time cut:"
-    print "\t" + "\n\t ||".join(get_long_drift_time_cut().split("||"))
-
-    print "\nget_few_channels_cmd:"
-    print "\t" + "\n\t +".join(get_few_channels_cmd().split("+"))
-
-    print "\nget_chargeEnergy_no_pz:"
-    print "\t", get_chargeEnergy_no_pz()
-
-    print "\nget_energy_weighted_drift_time:"
-    print "\t" + "\n\t +".join(get_energy_weighted_drift_time().split("+"))
-
-    print "\n get_multiplicity_cmd:"
-    print get_multiplicity_cmd()
-
-    print "\n get_single_strip_cut:"
-    print get_single_strip_cut()
-
-    print "\n get_corrected_energy_cmd:"
-    print get_corrected_energy_cmd()
-
-    #print "\n"+ get_long_drift_time_cut(energy_threshold=200,drift_time_low=7.0,drift_time_high=8.5)
-    print "\n"+ get_long_drift_time_cut(drift_time_high=8.5)
