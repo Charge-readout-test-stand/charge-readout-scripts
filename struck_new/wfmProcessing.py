968a31e71531877d9c07fc21a3637bf5e2b96047
Alexis Schubert <alexis4@stanford.edu>
2016-04-12 16:26:10 -0700

    * added isMC handling to basename and outfile creation methods * added more methods of PMT energy calculation

--- struck_new/wfmProcessing.py
+++ struck_new/wfmProcessing.py
@@ -26,12 +26,19 @@ from ROOT import EXOBaselineRemover
 from ROOT import EXORisetimeCalculation
 from ROOT import EXOSmoother
 from ROOT import EXOPoleZeroCorrection
+from ROOT import EXOExtremumFinder
+from ROOT import EXOTrapezoidalFilter
 
 import struck_analysis_parameters
 
 
 
-def do_draw(exo_wfm,title="", extra_wfm=None, vline=None):
+def do_draw(
+    exo_wfm,
+    title="", 
+    extra_wfm=None, 
+    extra_wfm2=None,
+    vline=None):
     
     if gROOT.IsBatch(): return
     # a canvas for drawing, if debugging:
@@ -41,17 +48,37 @@ def do_draw(exo_wfm,title="", extra_wfm=None, vline=None):
     hist = exo_wfm.GimmeHist("hist")
     hist.SetTitle(title)
     hist.SetLineWidth(2)
-    #hist.SetAxisRange(4,6)
+    #hist.SetAxisRange(7.5,9.5)
+    hist.SetMarkerStyle(8)
+    hist.SetMarkerSize(0.5)
     hist.Draw()
+    hist_max = hist.GetMaximum()
+    hist_min = hist.GetMinimum()
+    #hist.Draw("p")
     if extra_wfm:
         hist2 = extra_wfm.GimmeHist("hist2")
         hist2.SetLineColor(TColor.kBlue)
         hist2.Draw("same")
+        if hist2.GetMaximum() > hist_max:
+            hist_max = hist2.GetMaximum()
+        if hist2.GetMinimum() < hist_min:
+            hist_min = hist2.GetMinimum()
+
+    if extra_wfm2:
+        hist3 = extra_wfm2.GimmeHist("hist3")
+        hist3.SetLineColor(TColor.kRed)
+        hist3.Draw("same")
+        if hist3.GetMaximum() > hist_max:
+            hist_max = hist3.GetMaximum()
+        if hist3.GetMinimum() < hist_min:
+            hist_min = hist3.GetMinimum()
     if vline:
         line = TLine(vline, hist.GetMinimum(), vline, hist.GetMaximum())
         line.SetLineStyle(2)
         line.SetLineWidth(2)
         line.Draw()
+    hist.SetMaximum(hist_max*1.1)
+    hist.SetMinimum(hist_min-hist_max*0.1)
 
 
     canvas.Update()
@@ -64,18 +91,20 @@ def do_draw(exo_wfm,title="", extra_wfm=None, vline=None):
     
 
 
-def create_basename(filename):
+def create_basename(filename, isMC=False):
 
     # construct a basename to use as output file name
     basename = os.path.basename(filename)
     basename = os.path.splitext(basename)[0]
-    basename = "_".join(basename.split("_")[1:])
+    if not isMC:
+        # get rid of first underscore stuff:
+        basename = "_".join(basename.split("_")[1:])
     return basename
 
 
-def create_outfile_name(filename):
+def create_outfile_name(filename, isMC=False):
 
-    basename = create_basename(filename)
+    basename = create_basename(filename, isMC)
     out_filename = "tier3_%s.root" % basename
     return out_filename
 
@@ -89,6 +118,7 @@ def get_wfmparams(
     decay_time, 
     is_pmtchannel
 ):
+
     exo_wfm.SetSamplingFreq(sampling_freq_Hz/CLHEP.second)
     energy_wfm = EXODoubleWaveform(exo_wfm)
 
@@ -103,20 +133,20 @@ def get_wfmparams(
     baseline_remover.Transform(exo_wfm)
     baseline_mean = baseline_remover.GetBaselineMean()
     baseline_rms = baseline_remover.GetBaselineRMS()
+    #do_draw(energy_wfm, "after baseline_remover")
 
-    # measure energy before PZ correction
+    # measure energy before PZ correction -- use baseline_remover for this
     baseline_remover.SetStartSample(wfm_length - n_baseline_samples - 1)
     baseline_remover.Transform(exo_wfm, energy_wfm)
     energy = baseline_remover.GetBaselineMean()*calibration
     energy_rms = baseline_remover.GetBaselineRMS()*calibration
-    if is_pmtchannel: # for PMT channel, use GetMaxValue()
-        energy = exo_wfm.GetMaxValue()*calibration
-    #do_draw(energy_wfm, "after baseline_remover")
 
-    # measure energy before PZ correction, use 2x n_baseline_samples
+    # remove baseline using 2x n_baseline_samples
     baseline_remover.SetBaselineSamples(2*n_baseline_samples)
     baseline_remover.SetStartSample(0)
     baseline_remover.Transform(exo_wfm)
+
+    # measure energy before PZ correction, use 2x n_baseline_samples
     baseline_remover.SetStartSample(wfm_length - 2*n_baseline_samples - 1)
     baseline_remover.Transform(exo_wfm, energy_wfm)
     energy1 = baseline_remover.GetBaselineMean()*calibration
@@ -125,11 +155,12 @@ def get_wfmparams(
     # correct for exponential decay
     pole_zero = EXOPoleZeroCorrection()
     pole_zero.SetDecayConstant(decay_time)
-    pole_zero.Transform(exo_wfm, energy_wfm)
-    #do_draw(energy_wfm, "after PZ correction")
+    if not is_pmtchannel:
+        pole_zero.Transform(exo_wfm, energy_wfm)
+        do_draw(energy_wfm, "after PZ correction")
 
-    # measure energy after PZ correction
-    baseline_remover.SetBaselineSamples(n_baseline_samples)
+    # measure energy after PZ correction -- use baseline remover
+    baseline_remover.SetBaselineSamples(n_baseline_samples) # remove baseline
     baseline_remover.SetStartSample(wfm_length - n_baseline_samples - 1)
     baseline_remover.Transform(energy_wfm)
     energy_pz = baseline_remover.GetBaselineMean()*calibration
@@ -140,7 +171,9 @@ def get_wfmparams(
     baseline_remover.SetStartSample(0)
     baseline_remover.Transform(exo_wfm)
 
-    pole_zero.Transform(exo_wfm, energy_wfm)
+    # correct for exponential decay
+    if not is_pmtchannel:
+        pole_zero.Transform(exo_wfm, energy_wfm)
 
     # create calibrated wfm to be added to sum wfm
     calibrated_wfm = EXODoubleWaveform(energy_wfm)
@@ -150,6 +183,67 @@ def get_wfmparams(
     baseline_remover.Transform(energy_wfm)
     energy1_pz = baseline_remover.GetBaselineMean()*calibration
     energy_rms1_pz = baseline_remover.GetBaselineRMS()*calibration
+
+    if is_pmtchannel: # for PMT channel, use GetMaxValue()
+        extremum_finder = EXOExtremumFinder()
+        extremum_finder.SetFindMaximum(True)
+        extremum_finder.Transform(exo_wfm)
+        index_of_max = extremum_finder.GetTheExtremumPoint()
+        n_points = 9
+        avg = 0.0
+        n_used_points = 0
+        # average a few points around the maximum
+        for i in xrange(n_points):
+            index = i - n_points/2 + index_of_max
+
+            # handle max at early times:
+            if index < 0:
+                continue
+            n_used_points += 1
+
+            #print "i:", i
+            #print "index:", index
+            #print "wfm:", exo_wfm[index]
+            avg += exo_wfm[index]
+        avg /= n_used_points
+        energy = exo_wfm.GetMaxValue()*calibration
+        energy1 = avg*calibration
+
+        if not gROOT.IsBatch():
+            print "is PMT"
+            print "index_of_max:", index_of_max
+            print "max_val", avg*calibration
+            print "energy:", energy
+            print "decay_time:", decay_time
+
+        pz_wfm = EXODoubleWaveform(exo_wfm)
+        pole_zero.SetDecayConstant(decay_time)
+        pole_zero.Transform(exo_wfm, pz_wfm)
+        
+        trap_wfm = EXODoubleWaveform(exo_wfm)
+        trap_filter = EXOTrapezoidalFilter()
+        trap_filter.SetFlatTime(2.0*CLHEP.microsecond)
+        trap_filter.SetRampTime(4.0*CLHEP.microsecond)
+        trap_filter.Transform(pz_wfm, trap_wfm)
+        #trap_filter.Transform(exo_wfm, trap_wfm)
+        #trap_wfm /= 20.0
+        energy_pz = trap_wfm.GetMaxValue()
+
+        trap_filter.SetRampTime(2.0*CLHEP.microsecond)
+        trap_filter.Transform(pz_wfm, trap_wfm)
+        energy1_pz = trap_wfm.GetMaxValue()
+
+        do_draw(
+            #exo_wfm, 
+            #pz_wfm,
+            trap_wfm,
+            "after %i-sample baseline_remover" % baseline_remover.GetBaselineSamples(), 
+            #extra_wfm=trap_wfm,
+            extra_wfm=exo_wfm,
+            extra_wfm2=pz_wfm,
+            vline=index_of_max*exo_wfm.GetSamplingPeriod()/CLHEP.microsecond,
+        )
+
 
     return (
         baseline_mean, 
