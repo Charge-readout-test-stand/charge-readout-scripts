7ca11dd499f35a2eabebcecf87dd2372e9fbc045
Alexis Schubert <alexis4@stanford.edu>
2016-04-13 17:54:02 -0700

    added options for MC

--- struck/drawEnergyVsDriftTimeSlices.py
+++ struck/drawEnergyVsDriftTimeSlices.py
@@ -27,17 +27,15 @@ gStyle.SetTitleStyle(0)
 gStyle.SetTitleBorderSize(0)
 
 import struck_analysis_parameters
+import struck_analysis_cuts
 
 def process_file(filename, fit_results_filename):
     drift_velocity = struck_analysis_parameters.drift_velocity
 
     # options:
+    do_draw_fit_results = False
     #draw_cmd = "energy1_pz:(rise_time_stop99-trigger_time+0.020)"
     draw_cmd = "energy1_pz:(rise_time_stop99-trigger_time+0.020)*%s" % drift_velocity
-    single_strip_cut = struck_analysis_parameters.get_single_strip_cut(10.0)
-    selection = []
-    selection.append(single_strip_cut)
-    selection.append("channel<8")
 
     print "---> processing", filename
     basename = os.path.splitext(os.path.basename(filename))[0]
@@ -48,6 +46,18 @@ def process_file(filename, fit_results_filename):
         print "%i entries" % n_entries
     except AttributeError:
         print "could not get entries from tree"
+    isMC = False
+    tree.MCchargeEnergy
+    isMC = True
+
+    # selections:
+    single_strip_cut = struck_analysis_cuts.get_single_strip_cut(10.0, isMC)
+    selection = []
+    selection.append(single_strip_cut)
+    if isMC:
+        selection.append(struck_analysis_cuts.get_mc_channels_selection())
+    else:
+        selection.append("channel<8")
 
     print "draw_cmd:", draw_cmd
     selection = "&&".join(selection)
@@ -55,39 +65,40 @@ def process_file(filename, fit_results_filename):
     print "\n"
 
 
-    # grab info from json fit results:
-    print "--> processing fit results", fit_results_filename
-    json_file = file(fit_results_filename,'r')
-    fit_results = json.load(json_file) 
-    #print json.dumps(fit_results, sort_keys=True, indent=4)
-    graph = TGraphErrors()
-    graph.SetLineWidth(2)
-    keys = fit_results.keys()
-    keys.sort()
-    for i, drift_time in enumerate(keys):
-        values = fit_results[drift_time]
-        drift_time = float(drift_time)
-        centroid = float(values["centroid"])
-        centroid_err = float(values["centroid_err"])
-        sigma = float(values["sigma"])
-        fit_status = int(values["fit_status"])
-        dt = float(values["dt"])
-        dz = dt*drift_velocity
-        z=drift_time*drift_velocity
-        print "drift_time %i: %.1f:" % (i, drift_time)
-        print "\t centroid", centroid
-        print "\t sigma", sigma
-        print "\t dt", dt
-        print "\t dz", dz
-        print "\t z", z
-        print "\t fit_status", fit_status
-        if fit_status != 0:
-            print "BAD FIT!"
-        #    continue
-        i_point = graph.GetN()
-        graph.SetPoint(i_point,z+dz/2.0, centroid)
-        #graph.SetPointError(i_point,dz/2.0,sigma)
-        graph.SetPointError(i_point,dz/2.0,centroid_err)
+    if do_draw_fit_results:
+        # grab info from json fit results:
+        print "--> processing fit results", fit_results_filename
+        json_file = file(fit_results_filename,'r')
+        fit_results = json.load(json_file) 
+        #print json.dumps(fit_results, sort_keys=True, indent=4)
+        graph = TGraphErrors()
+        graph.SetLineWidth(2)
+        keys = fit_results.keys()
+        keys.sort()
+        for i, drift_time in enumerate(keys):
+            values = fit_results[drift_time]
+            drift_time = float(drift_time)
+            centroid = float(values["centroid"])
+            centroid_err = float(values["centroid_err"])
+            sigma = float(values["sigma"])
+            fit_status = int(values["fit_status"])
+            dt = float(values["dt"])
+            dz = dt*drift_velocity
+            z=drift_time*drift_velocity
+            print "drift_time %i: %.1f:" % (i, drift_time)
+            print "\t centroid", centroid
+            print "\t sigma", sigma
+            print "\t dt", dt
+            print "\t dz", dz
+            print "\t z", z
+            print "\t fit_status", fit_status
+            if fit_status != 0:
+                print "BAD FIT!"
+            #    continue
+            i_point = graph.GetN()
+            graph.SetPoint(i_point,z+dz/2.0, centroid)
+            #graph.SetPointError(i_point,dz/2.0,sigma)
+            graph.SetPointError(i_point,dz/2.0,centroid_err)
 
     # 2D hist for time vs. energy
     hist = TH2D("hist","",275,0,11*drift_velocity,100,300,1300)
@@ -109,17 +120,20 @@ def process_file(filename, fit_results_filename):
     plot_name = "driftDistVsEnergy"
     canvas.Print("%s.pdf" % plot_name)
     canvas.Print("%s.png" % plot_name)
-    print "%i ihist entries drawn" % n_entries
+    print "%i hist entries drawn" % n_entries
+    if not gROOT.IsBatch():
+        raw_input("pause")
 
-    # draw graph over hist
-    graph.Draw("p")
-    canvas.Update()
-    n_slices = len(fit_results.keys())
-    print "%i slices" % n_slices
-    plot_name = "%s_%islices" % (plot_name, n_slices)
-    canvas.Print("%s.pdf" % plot_name)
-    canvas.Print("%s.png" % plot_name)
-    raw_input("enter... ")
+    if do_draw_fit_results:
+        # draw graph over hist
+        graph.Draw("p")
+        canvas.Update()
+        n_slices = len(fit_results.keys())
+        print "%i slices" % n_slices
+        plot_name = "%s_%islices" % (plot_name, n_slices)
+        canvas.Print("%s.pdf" % plot_name)
+        canvas.Print("%s.png" % plot_name)
+        raw_input("enter... ")
 
 
 
@@ -133,7 +147,10 @@ if __name__ == "__main__":
         #fit_results_file = "fit_results_z_slices_overnight7thLXe_21.txt"
         fit_results_file = "fit_results_z_slices_overnight7thLXe_19.txt"
 
-        process_file( "overnight7thLXe.root", fit_results_file)
+        #data_file = "overnight7thLXe.root"
+        data_file = "/nfs/slac/g/exo_data4/users/alexis4/test-stand/mc/Bi207_Full_Ralph/tier3_5x/all_pcd_size_5x_dcoeff200.root"
+
+        process_file(data_file, fit_results_file)
 
     elif len(sys.argv) < 3:
         process_file(sys.argv[1], sys.argv[2])
