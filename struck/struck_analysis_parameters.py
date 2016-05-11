17f493a112b208133f5d71995bce3f4a326ab116
Alexis Schubert <alexis4@stanford.edu>
2016-05-11 14:34:47 -0700

    new W-value for updated MC physics list

--- struck/struck_analysis_parameters.py
+++ struck/struck_analysis_parameters.py
@@ -157,11 +157,15 @@ def is_tree_MC(tree):
 
 
 #Wvalue for Xenon
-#From EXODimensions.hh
+#From EXODimensions.hh:
 #const double W_VALUE_IONIZATION = 15.6*CLHEP::eV;
 #const double W_VALUE_LXE_EV_PER_ELECTRON = 18.7*CLHEP::eV;
 # from NumTE from 1 MeV electrons: 1e3/4.96561538461538439e+04 
-Wvalue = 20.138 #eV needed to make 1e- 
+#Wvalue = 20.138 #eV needed to make 1e- # from old SU LXe setup
+#Wvalue = 22.1225754074916985 # eV, for gammas @ 936 V/cm, nEXO MC, 10k events
+# new physics list 11 May 2016:
+#Wvalue = 21.1403194621 # eV, e-s @ 936 V/cm in SU LXe setup
+Wvalue = 22.0043657088 # eV, gammas @ 936 V/cm in SU LXe setup, 46k events
 
 # values from Peihao, 31 Oct 2015:
 decay_time_values = {}
