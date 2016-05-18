3ead29c952cf7d9971f43273cb221fbc6bbcaca5
Alexis Schubert <alexis4@stanford.edu>
2016-05-17 20:07:02 -0700

    working on cuts, printed formatting

--- mc/compareDigiMCtoStruckData.py
+++ mc/compareDigiMCtoStruckData.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 """
+# FIXME? change from TChain to adding to hists? this took 8 hours last time!!
 This script draws a spectrum from a root tree of digitized nEXO_MC results.
 
 Arguments: [MC root filename] [tier3 Struck data]
@@ -43,18 +44,24 @@ from struck import struck_analysis_cuts
 from struck import struck_analysis_parameters
 
 
-def process_file(mc_filename, struck_filename):
+def process_file(
+    mc_filename, 
+    struck_filename,
+    i_channel=None,
+    draw_cmd="energy1_pz",
+    #draw_cmd = "chargeEnergy",
+    do_use_drift_time_cut=True,
+    do_use_single_strip_cut=True,
+):
 
-    print "processing file: ", mc_filename
+    print "====> processing file: ", mc_filename
+    print "\t i_channel:", i_channel
+    print "\t draw_cmd:", draw_cmd
+    print "\t do_use_drift_time_cut:", do_use_drift_time_cut
+    print "\t do_use_single_strip_cut:", do_use_single_strip_cut
 
     # options:
-    #draw_cmd = "chargeEnergy"
-    draw_cmd = "energy1_pz"
     chargeEnergy_cut = "(%s>0)" % draw_cmd
-
-    do_use_drift_time_cut = True
-    do_use_single_strip_cut = True
-    i_channel = 4 # None # 5
 
     # drift-time cut info
     drift_time_low = struck_analysis_parameters.drift_time_threshold 
@@ -64,10 +71,14 @@ def process_file(mc_filename, struck_filename):
     drift_time_high = 9.0
     #drift_time_high = 11.0 # microseconds
 
+    #struck_draw_cmd = draw_cmd 
+    struck_energy_multiplier = 0.92
+    struck_draw_cmd = "%s*%s" % (draw_cmd, struck_energy_multiplier) # until e-calibration is fixed
+    print "\t struck_draw_cmd:", struck_draw_cmd
+
     # this isn't the right scaling anymore, since we are using the pre-scaler
     # which throws away some data
-    source_activity_Bq = 400.0 # activity of 207-Bi source installed on cathode on Aug 7 2015
-
+    #source_activity_Bq = 400.0 # activity of 207-Bi source installed on cathode on Aug 7 2015
 
     # estimate of ionization resolution comes from here:
     # https://confluence.slac.stanford.edu/download/attachments/162955571/EXO-200_public_material_28Feb2014.pptx?version=1&modificationDate=1394478396000&api=v2
@@ -88,7 +99,7 @@ def process_file(mc_filename, struck_filename):
     #sigma_keV = 50.0 # testing
 
 
-    print "\tsigma_keV", sigma_keV
+    print "\t sigma_keV", sigma_keV
 
     mc_files = glob.glob(mc_filename)
 
@@ -103,12 +114,12 @@ def process_file(mc_filename, struck_filename):
         mc_tree.Add(mc_filename)
         n_entries = mc_tree.GetEntries()
         basename = os.path.commonprefix(mc_files)
-    print "\t%.2e events in MC tree" % mc_tree.GetEntries()
+    print "\t %.2e events in MC tree" % mc_tree.GetEntries()
 
     # construct a basename from the input filename
     basename = os.path.basename(basename) # get rid of file path
     basename = os.path.splitext(basename)[0] # get rid of file suffix
-    print "basename:", basename 
+    print "\t basename:", basename 
 
 
     plot_name = "%s_spectrum_sigma_%i_keV" % (
@@ -125,10 +136,8 @@ def process_file(mc_filename, struck_filename):
     if "energy1_pz" in draw_cmd:
         plot_name += "_E1PZ"
         if i_channel != None:
-            plot_name += "_ch%i" % i_channel
-    print "plot_name:", plot_name
-
-
+            plot_name += "_ch%i_singleMcCh" % i_channel
+    print "\t plot_name:", plot_name
 
     # make a histogram to hold MC energies
     hist = TH1D("hist", "", 250, 0, 2500)
@@ -155,11 +164,13 @@ def process_file(mc_filename, struck_filename):
                     drift_time_high))
         else:
             struck_selection.append(struck_analysis_cuts.get_drift_time_cut(
+                energy_threshold=200.0/struck_energy_multiplier,
                 drift_time_low=drift_time_low,
                 drift_time_high=drift_time_high,
             ))
     if do_use_single_strip_cut:
-        part = struck_analysis_cuts.get_single_strip_cut()
+        part = struck_analysis_cuts.get_single_strip_cut(
+            energy_threshold=10.0/struck_energy_multiplier)
         struck_selection.append(part)
     if "energy1_pz" in draw_cmd:
         if i_channel != None:
@@ -167,11 +178,9 @@ def process_file(mc_filename, struck_filename):
         else:
             struck_selection.append(struck_analysis_cuts.get_channel_selection())
     struck_selection = "&&".join(struck_selection)
-    print "struck_selection:", struck_selection
+    print "struck_selection:"
+    print "\t", "\n\t || ".join(struck_selection.split("||"))
 
-    #struck_draw_cmd = draw_cmd 
-    struck_draw_cmd = "%s*0.92" % draw_cmd# until e-calibration is fixed
-    print "struck_draw_cmd:", struck_draw_cmd
 
     # open the struck file and get its entries
     print "processing file: ", struck_filename
@@ -183,7 +192,7 @@ def process_file(mc_filename, struck_filename):
         struck_selection,
         "goff"
     )
-    print "\t%.1e struck entries" % struck_entries
+    print "\t%.1e struck entries drawn" % struck_entries
 
     # set MC color & style
     mc_tree.SetLineColor(TColor.kRed)
@@ -209,10 +218,14 @@ def process_file(mc_filename, struck_filename):
         part = struck_analysis_cuts.get_single_strip_cut(isMC=True)
         mc_selection.append(part)
     if "energy1_pz" in draw_cmd:
-        mc_selection.append(struck_analysis_cuts.get_channel_selection(isMC=True))
-        #mc_selection.append("channel==16")
+        if i_channel != None:
+            mc_channel = struck_analysis_parameters.struck_to_mc_channel_map[i_channel]
+            mc_selection.append("channel==%i" % mc_channel)
+        else:
+            mc_selection.append(struck_analysis_cuts.get_channel_selection(isMC=True))
     mc_selection = "&&".join(mc_selection)
-    print "mc_selection:", mc_selection
+    print "mc_selection:"
+    print "\t", "\n\t || ".join(mc_selection.split("||"))
 
     if sigma_keV == 0: # we don't add sigma anymore, since it's added in tier3
 
@@ -225,7 +238,7 @@ def process_file(mc_filename, struck_filename):
             mc_selection,
             "goff"
         )
-        print "\t%.1e MC entries" % mc_entries
+        print "\t%.1e MC entries drawn" % mc_entries
 
 
     else: # for non-zero sigma
@@ -242,27 +255,20 @@ def process_file(mc_filename, struck_filename):
         resolution_hist.SetXTitle("energy [keV]")
 
 
-
         # fill the hist in a loop so we can smear the energy resolution
         print "filling hist..."
         for i_entry in xrange(mc_tree.GetEntries()):
-
             mc_tree.GetEntry(i_entry)
-            
             #if (mc_tree.chargeEnergy > 0.0):
             if True:
-
                 mc_energy_keV = mc_tree.chargeEnergy
-
                 smearing_keV = generator.Gaus()*sigma_keV
-
                 # print some debugging info:
                 #print "mc_energy_keV: %.2f | smearing_keV: %.2f | sum: %.2f" % (
                 #    mc_energy_keV,
                 #    smearing_keV,
                 #    mc_energy_keV+smearing_keV,
                 #)
-
                 hist.Fill(mc_energy_keV + smearing_keV)
                 resolution_hist.Fill(smearing_keV)
 
@@ -291,11 +297,73 @@ def process_file(mc_filename, struck_filename):
     canvas.SetLogy(1)
     canvas.SetGrid(1,1)
 
-    # given the estimated activity of the source
-    seconds_per_minute = 60.0
-    struck_height = hist_struck.GetBinContent(hist_struck.FindBin(570))
-    mc_height = hist.GetBinContent(hist.FindBin(570))
-    scale_factor = struck_height/mc_height
+    # scale MC to match struck data, many different methods... 
+
+    peak_energy = 570
+    # struck_height used for scaling (sometimes) and also for plotting
+    struck_height = hist_struck.GetBinContent(hist_struck.FindBin(peak_energy))
+    if False: # use peak height
+        print "scale factor based on height at %i keV" % peak_energy
+        mc_height = hist.GetBinContent(hist.FindBin(peak_energy))
+        scale_factor = struck_height/mc_height
+
+    integral_energy_min = 300.0
+    integral_energy_max = 2500.0
+    if False: # use integral counts
+        # normalize each channel independently
+        min_bin = hist.FindBin(integral_energy_min)
+        max_bin = hist.FindBin(integral_energy_max) 
+        mc_counts = hist.Integral(min_bin, max_bin)
+        struck_counts = hist_struck.Integral(min_bin, max_bin)
+        scale_factor = struck_counts/mc_counts
+        print "scale factor based on spectrum integral above %i keV" % integral_energy_min
+
+    else:
+        # normalize based on number of counts in all charge channels, use same
+        # normalization for each channel
+        print "scale factor based on integral %s above %i keV with selections" % (
+            draw_cmd, integral_energy_min)
+        do_use_n_events = True # whether to use n events or n channel hits
+        if do_use_n_events:
+            mc_selection = "%s>%s && %s<%s" % (
+                draw_cmd, integral_energy_min,
+                draw_cmd, integral_energy_max,
+            )
+            mc_selection+= "&&" + struck_analysis_cuts.get_drift_time_cut(
+                energy_threshold=200.0,
+                drift_time_low=drift_time_low,
+                drift_time_high=drift_time_high,
+                isMC=True
+            )
+        else:
+            print "need drift time cut!!!"
+            mc_selection = "%s>%s && %s<%s && %s" % (
+                draw_cmd, integral_energy_min,
+                draw_cmd, integral_energy_max,
+                struck_analysis_cuts.get_channel_selection(isMC=True))
+
+        print "scale factor MC selection:", mc_selection
+        mc_counts = mc_tree.Draw(draw_cmd,mc_selection,"goff")
+        print "mc_counts:", mc_counts
+        if do_use_n_events:
+            struck_selection = "%s>%s && %s<%s" % ( 
+                struck_draw_cmd, integral_energy_min,
+                struck_draw_cmd, integral_energy_max,)
+            struck_selection += "&&"+struck_analysis_cuts.get_drift_time_cut(
+                energy_threshold=200.0/struck_energy_multiplier,
+                drift_time_low=drift_time_low,
+                drift_time_high=drift_time_high,
+            )
+        else:
+            struck_selection = "%s>%s && %s<%s && %s" % ( 
+                struck_draw_cmd, integral_energy_min,
+                struck_draw_cmd, integral_energy_max,
+                struck_analysis_cuts.get_channel_selection())
+        print "scale factor Struck selection:", struck_selection
+        struck_counts = struck_tree.Draw(struck_draw_cmd,struck_selection,"goff")
+        print "struck_counts:", struck_counts
+        scale_factor = struck_counts*1.0/mc_counts
+
     #scale_factor *= 0.5 # extra offset for viewing
     print "scale_factor", scale_factor
     hist.Scale(scale_factor)
@@ -314,18 +382,18 @@ def process_file(mc_filename, struck_filename):
         if "energy1_pz" in draw_cmd and i_channel != None:
             mc_title += "Ch%i " % i_channel
         mc_title += "(%.1e cts x %.2f)" % (hist.GetEntries(), scale_factor)
-        legend.AddEntry(hist, mc_title)
+        legend.AddEntry(hist, mc_title,"f")
     else:
-        legend.AddEntry(hist, "MC, #sigma_{addl}=%i keV" % sigma_keV, "fl") 
-    legend.AddEntry(hist_struck, "Struck data (%.1e cts)" % hist_struck.GetEntries(), "fl")
+        legend.AddEntry(hist, "MC, #sigma_{addl}=%i keV" % sigma_keV, "f") 
+    legend.AddEntry(hist_struck, "Struck data (%.1e cts)" % hist_struck.GetEntries(), "f")
 
     hist_struck.Draw()
     hist.Draw("same") 
     #hist_struck.Draw("same")
-    print "%i struck entries" % hist_struck.GetEntries()
-    print "%i mc entries" % hist.GetEntries()
+    print "%i entries in hist_struck" % hist_struck.GetEntries()
+    print "%i entries in MC hist" % hist.GetEntries()
 
-    pave_text = TPaveText(0.7, 0.8, 0.9, 0.9, "NDC")
+    pave_text = TPaveText(0.65, 0.8, 0.9, 0.9, "NDC")
     pave_text.SetFillColor(0)
     pave_text.SetFillStyle(0)
     pave_text.SetBorderSize(0)
@@ -379,10 +447,14 @@ if __name__ == "__main__":
 
     # 7th LXe
     #mc_file = "/nfs/slac/g/exo_data4/users/alexis4/test-stand/mc/old_Bi207_Full_Ralph/tier3_5x/all_dcoef200_pcd_size_5x.root"
-    mc_file = "/nfs/slac/g/exo_data4/users/alexis4/test-stand/mc/Bi207_Full_Ralph_dcoeff50/tier3/tier3*.root"
+    mc_file = "/nfs/slac/g/exo_data4/users/alexis4/test-stand/mc/Bi207_Full_Ralph_dcoeff50/tier3/tier3_*1000*.root"
     data_file = "/u/xo/alexis4/test-stand/2016_03_07_7thLXe/tier3_external/overnight7thLXe.root" 
 
-    process_file(mc_file, data_file)
+    process_file(mc_file, data_file,draw_cmd="chargeEnergy", do_use_single_strip_cut=False)
+    process_file(mc_file, data_file,draw_cmd="chargeEnergy")
+    process_file(mc_file, data_file,i_channel=None)
+    for i_channel in xrange(8):
+        process_file(mc_file, data_file,i_channel=i_channel)
 
 
 
