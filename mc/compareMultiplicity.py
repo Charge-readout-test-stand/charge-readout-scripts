eaa6db7fdf5421caecb5b40405a63858480231d5
Alexis Schubert <alexis4@stanford.edu>
2016-05-19 15:32:41 -0700

    plotting changes

--- mc/compareMultiplicity.py
+++ mc/compareMultiplicity.py
@@ -9,12 +9,14 @@ import sys
 import glob
 
 from ROOT import gROOT
-#gROOT.SetBatch(True) # comment out to run interactively
+gROOT.SetBatch(True) # comment out to run interactively
 from ROOT import TH1D
+from ROOT import TH1
 from ROOT import TFile
 from ROOT import TCanvas
 from ROOT import TColor
 from ROOT import TLegend
+from ROOT import TPaveText
 from ROOT import gStyle
 
 from struck import struck_analysis_parameters
@@ -37,9 +39,9 @@ def process_file(
     # options:
     struck_energy_multiplier = struck_analysis_parameters.struck_energy_multiplier
     energy_threshold = 10.0 # for each channel
-    charge_energy_low_threshold = 700.0
+    charge_energy_low_threshold = 470.0
     #charge_energy_low_threshold = 400.0
-    charge_energy_high_threshold = 1500.0
+    charge_energy_high_threshold = 670.0
     hist_max = 9.5
     hist_min = -0.5
     #n_bins = int((hist_max - hist_min)*1.0)*(len(mc_filenames)+2)
@@ -80,7 +82,30 @@ def process_file(
     hist_struck.SetFillColor(TColor.kBlue)
     hist_struck.SetFillStyle(3004)
     hist_struck.SetLineWidth(2)
-    struck_channels = hist_struck.Clone("struck_channels")
+
+    # set up hists
+    hist_struck.SetXTitle("Number of strips with E > %s keV" % energy_threshold)
+        
+
+    text = "events with total energy %s to %s keV" % (
+            charge_energy_low_threshold,
+            charge_energy_high_threshold,
+    )
+    pave_text = TPaveText(0.4, 0.8, 0.9, 0.9, "NDC")
+    pave_text.SetFillColor(0)
+    pave_text.SetFillStyle(0)
+    pave_text.SetBorderSize(0)
+    pave_text.AddText(text)
+
+    n_chargechannels = struck_analysis_parameters.n_chargechannels
+    struck_channels = TH1D("struck_channels","", n_chargechannels, 0, n_chargechannels)
+    mc_channels = struck_channels.Clone("mc_channels")
+    struck_channels.Sumw2()
+    struck_channels.SetLineColor(TColor.kBlue)
+    struck_channels.SetFillColor(TColor.kBlue)
+    struck_channels.SetFillStyle(3004)
+    struck_channels.SetLineWidth(2)
+    struck_channels.SetXTitle("charge channels hit with > 10 keV")
 
     # open the struck file and get its entries
     print "processing Struck file: ", struck_filename
@@ -124,20 +149,19 @@ def process_file(
     legend.SetFillColor(0)
     legend.SetNColumns(2)
 
-    # make a histogram to hold MC multiplicity
-    mc_hist = TH1D("hist", "", n_bins, hist_min, hist_max)
-    hist = mc_hist
-    hist.Sumw2()
     color = struck_analysis_parameters.get_colors()[0]
-    hist.SetLineColor(color)
-    #hist.SetFillColor(color)
-    #hist.SetFillStyle(3004)
-    hist.SetMarkerStyle(24)
-    hist.SetMarkerSize(1.0)
-    hist.SetMarkerColor(color)
-    hist.SetLineWidth(2)
-    mc_channels = mc_hist.Clone("mc_channels")
+    color = TColor.kRed
 
+    # make a hists for MC multiplicity, channels
+    mc_hist = TH1D("hist", "", n_bins, hist_min, hist_max)
+    for hist in [mc_hist, mc_channels]: # format these hists...
+        print hist.GetName()
+        hist.Sumw2()
+        hist.SetLineColor(color)
+        hist.SetMarkerStyle(24)
+        hist.SetMarkerSize(1.0)
+        hist.SetMarkerColor(color)
+        hist.SetLineWidth(2)
 
     # a weird command to map MC channel numbers to integers 0 to 7:
     mc_channel_draw_cmd = "channel-15-26*(channel>40)>>+ %s" % mc_channels.GetName()
@@ -155,14 +179,14 @@ def process_file(
         mc_file = TFile(mc_filename)
         mc_tree = mc_file.Get("tree")
         try:
-            print "\t%.2e events in MC tree" % mc_tree.GetEntries()
+            print "\t%.2e events in MC tree, file %i of %i" % (mc_tree.GetEntries(), mc_i, len(mc_filenames))
         except AttributeError:
             print "\t skipping this file..."
             continue
 
         mc_hist.GetDirectory().cd()
         mc_entries = mc_tree.Draw(
-            "%s >>+ %s" % (mc_draw_cmd, hist.GetName()),
+            "%s >>+ %s" % (mc_draw_cmd, mc_hist.GetName()),
             mc_selection,
             "goff"
         )
@@ -177,9 +201,9 @@ def process_file(
         print "\t%s: %.2e MC channel entries drawn" % (mc_basename, mc_entries)
         print "\t%s: %.2e MC channels hist entries" % (mc_basename, mc_hist.GetEntries())
 
-    hist.Scale(1.0/mc_hist.GetEntries())
+    mc_hist.Scale(1.0/mc_hist.GetEntries())
     mc_channels.Scale(1.0/mc_channels.GetEntries())
-    legend.AddEntry(hist, mc_basename, "lp")
+    legend.AddEntry(mc_hist, mc_basename, "lp")
 
     # set up a canvas
     canvas = TCanvas("canvas","")
@@ -188,14 +212,16 @@ def process_file(
     hist_struck.Scale(1.0/struck_entries)
     struck_channels.Scale(1.0/struck_channels.GetEntries())
 
-    hist_struck.SetXTitle("Number of strips with E > %s keV" % energy_threshold)
 
     legend.AddEntry(hist_struck, "Struck data", "f")
 
     hist_struck.SetMaximum(1.0)
     hist_struck.Draw("hist")
     mc_hist.Draw("p same") 
+    #mc_hist.Draw("hist")
+    #hist_struck.Draw("p same")
     mc_hist.GetEntries()
+    pave_text.Draw()
     legend.Draw()
     #hist_struck.Draw("same")
     print "%i struck entries" % hist_struck.GetEntries()
@@ -229,29 +255,39 @@ def process_file(
         energy_threshold,
     )
 
-    canvas.Update()
-    canvas.Print("%s_multiplicity_lin.pdf" % plot_name)
-
     canvas.SetLogy(1)
     canvas.Update()
     canvas.Print("%s_multiplicity_log.pdf" % plot_name)
+
+    canvas.SetLogy(0)
+    canvas.Update()
+    canvas.Print("%s_multiplicity_lin.pdf" % plot_name)
 
     if not gROOT.IsBatch():
         canvas.Update()
         raw_input("enter to continue...")
 
+    # use labels for X axis
+    xaxis = struck_channels.GetXaxis()
+    for channel, value in enumerate(struck_analysis_parameters.charge_channels_to_use):
+        if value != 1: continue
+        label = struck_analysis_parameters.channel_map[channel]
+        i_bin = channel+1
+        #print "bin %i: channel %i | %s" % (i_bin, channel, label)
+        xaxis.SetBinLabel(i_bin, label)
     struck_channels.SetMaximum(1.0)
     struck_channels.Draw("hist")
     mc_channels.Draw("p same")
+    pave_text.Draw()
     legend.Draw()
-
-    canvas.SetLogy(0)
-    canvas.Update()
-    canvas.Print("%s_channels_lin.pdf" % plot_name)
 
     canvas.SetLogy(1)
     canvas.Update()
     canvas.Print("%s_channels_log.pdf" % plot_name)
+
+    canvas.SetLogy(0)
+    canvas.Update()
+    canvas.Print("%s_channels_lin.pdf" % plot_name)
 
     if not gROOT.IsBatch():
         canvas.Update()
@@ -283,9 +319,12 @@ if __name__ == "__main__":
         #"/nfs/slac/g/exo_data4/users/alexis4/test-stand/mc/Bi207_Full_Ralph/tier3_2x/all_dcoef200_2x.root",
     ]
     mc_files = glob.glob(
-        #"/nfs/slac/g/exo_data4/users/alexis4/test-stand/mc/Bi207_Full_Ralph_dcoeff50/tier3/tier3_*00*.root"
-        "/nfs/slac/g/exo_data4/users/alexis4/test-stand/mc/Bi207_Full_Ralph_dcoeff70/tier3/tier3_*.root"
+        "/nfs/slac/g/exo_data4/users/alexis4/test-stand/mc/Bi207_Full_Ralph_dcoeff50/tier3/tier3_*00*.root"
+        #"/nfs/slac/g/exo_data4/users/alexis4/test-stand/mc/Bi207_Full_Ralph_dcoeff70/tier3/tier3_*.root"
+        #"/nfs/slac/g/exo_data4/users/alexis4/test-stand/mc/Bi207_Full_Ralph_dcoeff49/tier3/tier3_*.root"
+        #"/nfs/slac/g/exo_data4/users/alexis4/test-stand/mc/Bi207_Full_Ralph_dcoeff51/tier3/tier3_*.root"
     )
+    mc_files.sort()
 
     process_file(
         data_file,
