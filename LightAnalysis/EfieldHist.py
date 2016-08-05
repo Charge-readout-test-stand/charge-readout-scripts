7e58305a7304136102cd48184f1e88c38eee3b81
Manisha Patel <manisha2@rhel6-64c.slac.stanford.edu>
2016-08-05 14:32:20 -0700

    e field hist

--- LightAnalysis/EfieldHist.py
+++ LightAnalysis/EfieldHist.py
@@ -7,18 +7,21 @@ from ROOT import TFile
 from ROOT import TCanvas
 c1 = TCanvas()
 from ROOT import TH2D
+from ROOT import gStyle
+gStyle.SetOptStat(0)
 
 def main(EfieldHist):
 	print "Processing file:", EfieldHist
 
 	#to find number of columns in file
-	TPC_Efield_RegGrid_no_Nan = file(EfieldHist)
+        TPC_E_RegGrid_20160718_v2_noNaN = file(EfieldHist)
 
 	branchList = [
 		"x",
 		"y",
 		"Ex",
 		"Ey",
+                "V"
 		]
 
 	branchDescriptor = ":".join(branchList)
@@ -33,15 +36,29 @@ def main(EfieldHist):
 	tree.ReadFile(EfieldHist, branchDescriptor)
 	print "...done"
 
-	Efieldhist = TH2D("Efieldhist", "Ey vs. Ex", 2900, -145.475, 144.525, 1800, -80, 100)
-	tree.Draw("y:x-125.475>>Efieldhist", "sqrt(Ex*Ex + Ey*Ey)", "goff")
-	Efieldhist.SetXTitle("Ex")
-	Efieldhist.SetYTitle("Ey")
+	Efieldhist = TH2D("Efieldhist", "", 2900, -145.475, 144.525, 1800, -80, 100)
+        #Efieldhist = TH2D("Efieldhist", "", 2100, -105, 105, 250, -5, 20)
+        #tree.Draw("y-10.54:x-125.475>>Efieldhist", "(sqrt(Ex*Ex + Ey*Ey)/100)*((sqrt(Ex*Ex + Ey*Ey)/100)>950)", "goff")
+        tree.Draw("y-10.54:x-125.475>>Efieldhist", "(sqrt(Ex*Ex + Ey*Ey)/100)", "goff")
+        tree.Write()
+        Efieldhist.Write()
+	Efieldhist.SetXTitle("Rho (mm)")
+	Efieldhist.SetYTitle("Z (mm)")
+        Efieldhist.GetXaxis().CenterTitle()
+        Efieldhist.GetYaxis().CenterTitle()
+        #Efieldhist.SetMinimum(950)
+
+
 	Efieldhist.Draw("colz")
+        iBin = Efieldhist.FindBin(0, 18)
+        #int pause
+        #print pause
 #interpolation method:	T = griddata((xi, yi), (x, y), method='cubic')
-	c1.SetGrid()	
+	c1.SetGrid()
+	c1.SetLogz(0)	
 	c1.Print("EfieldHist.png")
 	c1.Update()
+	print Efieldhist.GetBinContent(iBin)
 	raw_input("press enter")
 if __name__ == "__main__":
 
@@ -52,3 +69,4 @@ if __name__ == "__main__":
     filename = sys.argv[1]
 
     main(filename)
+
