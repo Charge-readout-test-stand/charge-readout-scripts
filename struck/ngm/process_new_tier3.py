1a0bb727bd59d3a7b6e695cd03b69d4138deaae2
lxereadout <lxe.readout@gmail.com>
2016-08-18 08:42:47 -0700

    using commands since python loop seemed to have memory leak

--- struck/ngm/process_new_tier3.py
+++ struck/ngm/process_new_tier3.py
@@ -1,11 +1,15 @@
 
 import os
 import sys
+import inspect
+import commands
 
 from struck import generateTier3Files
 
 
 def process_files(filenames):
+
+    script_name = "%s.py" % os.path.splitext(inspect.getfile(generateTier3Files))[0]
 
     for i, filename in enumerate(filenames):
         
@@ -14,15 +18,23 @@ def process_files(filenames):
         basename = os.path.basename(filename) # drop the directory structure
         basename = "_".join(basename.split("_")[1:])  # drop the tier1_
         tier3_name = "tier3_%s" % basename
-        print tier3_name
 
         # if tier3 file doesn't exist, create it:
         if os.path.isfile(tier3_name):
             print "==> skipping"
 
         else:
+            filename = os.path.abspath(filename)
+            log_name = "log_%s.out" % os.path.splitext(tier3_name)[0]
             print "processing!"
-            generateTier3Files.process_file(filename) # do tier1 conversion
+            cmd = "nice python %s %s > %s 2>&1" % (script_name, filename, log_name)
+            print cmd
+            output = commands.getstatusoutput(cmd)
+            if output[0] != 0:
+                print output[1]
+
+            # this still leaks memory
+            #generateTier3Files.process_file(filename) # do tier1 conversion
             
 
 if __name__ == "__main__":
