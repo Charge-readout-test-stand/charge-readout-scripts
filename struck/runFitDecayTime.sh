128c2e7f0bd72858b2996120dc21d4acadf5e905
lxereadout <lxe.readout@gmail.com>
2016-08-19 00:30:59 -0700

    Decay Constant Fitter added

---
+++ struck/runFitDecayTime.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+python fitDecayTime.py 1 /home/teststand/2016_08_15_8th_LXe_overnight/tier1/tier1_SIS3316Raw_2016081602*root 
+python fitDecayTime.py 2 /home/teststand/2016_08_15_8th_LXe_overnight/tier1/tier1_SIS3316Raw_2016081602*root 
+python fitDecayTime.py 3 /home/teststand/2016_08_15_8th_LXe_overnight/tier1/tier1_SIS3316Raw_2016081602*root 
+python fitDecayTime.py 4 /home/teststand/2016_08_15_8th_LXe_overnight/tier1/tier1_SIS3316Raw_2016081602*root 
+python fitDecayTime.py 5 /home/teststand/2016_08_15_8th_LXe_overnight/tier1/tier1_SIS3316Raw_2016081602*root 
+python fitDecayTime.py 6 /home/teststand/2016_08_15_8th_LXe_overnight/tier1/tier1_SIS3316Raw_2016081602*root 
+python fitDecayTime.py 7 /home/teststand/2016_08_15_8th_LXe_overnight/tier1/tier1_SIS3316Raw_2016081602*root 
+python fitDecayTime.py 8 /home/teststand/2016_08_15_8th_LXe_overnight/tier1/tier1_SIS3316Raw_2016081602*root 
+python fitDecayTime.py 9 /home/teststand/2016_08_15_8th_LXe_overnight/tier1/tier1_SIS3316Raw_2016081602*root 
+python fitDecayTime.py 10 /home/teststand/2016_08_15_8th_LXe_overnight/tier1/tier1_SIS3316Raw_2016081602*root 
+python fitDecayTime.py 11 /home/teststand/2016_08_15_8th_LXe_overnight/tier1/tier1_SIS3316Raw_2016081602*root 
+python fitDecayTime.py 12 /home/teststand/2016_08_15_8th_LXe_overnight/tier1/tier1_SIS3316Raw_2016081602*root 
+python fitDecayTime.py 13 /home/teststand/2016_08_15_8th_LXe_overnight/tier1/tier1_SIS3316Raw_2016081602*root 
+python fitDecayTime.py 14 /home/teststand/2016_08_15_8th_LXe_overnight/tier1/tier1_SIS3316Raw_2016081602*root 
+python fitDecayTime.py 15 /home/teststand/2016_08_15_8th_LXe_overnight/tier1/tier1_SIS3316Raw_2016081602*root 
+python fitDecayTime.py 16 /home/teststand/2016_08_15_8th_LXe_overnight/tier1/tier1_SIS3316Raw_2016081602*root 
+python fitDecayTime.py 17 /home/teststand/2016_08_15_8th_LXe_overnight/tier1/tier1_SIS3316Raw_2016081602*root 
+python fitDecayTime.py 18 /home/teststand/2016_08_15_8th_LXe_overnight/tier1/tier1_SIS3316Raw_2016081602*root 
+python fitDecayTime.py 19 /home/teststand/2016_08_15_8th_LXe_overnight/tier1/tier1_SIS3316Raw_2016081602*root 
+python fitDecayTime.py 20 /home/teststand/2016_08_15_8th_LXe_overnight/tier1/tier1_SIS3316Raw_2016081602*root 
+python fitDecayTime.py 21 /home/teststand/2016_08_15_8th_LXe_overnight/tier1/tier1_SIS3316Raw_2016081602*root 
+python fitDecayTime.py 22 /home/teststand/2016_08_15_8th_LXe_overnight/tier1/tier1_SIS3316Raw_2016081602*root 
+python fitDecayTime.py 23 /home/teststand/2016_08_15_8th_LXe_overnight/tier1/tier1_SIS3316Raw_2016081602*root 
+python fitDecayTime.py 24 /home/teststand/2016_08_15_8th_LXe_overnight/tier1/tier1_SIS3316Raw_2016081602*root 
+python fitDecayTime.py 25 /home/teststand/2016_08_15_8th_LXe_overnight/tier1/tier1_SIS3316Raw_2016081602*root 
+python fitDecayTime.py 26 /home/teststand/2016_08_15_8th_LXe_overnight/tier1/tier1_SIS3316Raw_2016081602*root 
+python fitDecayTime.py 27 /home/teststand/2016_08_15_8th_LXe_overnight/tier1/tier1_SIS3316Raw_2016081602*root 
+python fitDecayTime.py 28 /home/teststand/2016_08_15_8th_LXe_overnight/tier1/tier1_SIS3316Raw_2016081602*root 
+python fitDecayTime.py 29 /home/teststand/2016_08_15_8th_LXe_overnight/tier1/tier1_SIS3316Raw_2016081602*root 
+python fitDecayTime.py 30 /home/teststand/2016_08_15_8th_LXe_overnight/tier1/tier1_SIS3316Raw_2016081602*root 
+
+
+
+
+
