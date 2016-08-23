a7486eaaef06c439717460da90804f8aff421201
alexis-s <alexis4@stanford.edu>
2016-08-22 18:09:40 -0700

    now creating a shell script for each job

--- struck/submitPythonJobsLLNL.py
+++ struck/submitPythonJobsLLNL.py
@@ -47,17 +47,34 @@ def main(python_script, filenames, options="", verbose=True):
 # sample LLNL command (from nEXO MC)
 #msub -A afqn -m abe -V -N mc$j -o $OUT_DIR/log/blog_$DIFF.job${j} -j oe -q pbatch -l walltime=12:00:00 $OUT_DIR/jobs_hold/doall${j}.script
 
+# https://computing.llnl.gov/tutorials/linux_clusters/
+
+
+        batch_script_name = "script_%s.sh" % basename
+        batch_script = open(batch_script_name,'w')
+        batch_script.write("#!/bin/bash \n")
+        batch_script.write('printenv \n')
+        batch_script.write('python %(python_script)s %(options)s %(filename)s \n' % {
+          "python_script": python_script,
+          "options": options,
+          "filename": filename,
+        })
+        batch_script.close()
+
+        print "batch_script_name:", batch_script_name
+
         script = """
-    bmsub \\
+    msub \\
       -A afqn \\
       -m abe \\
       -V \\
-      -q %(queue)s \\
-      -walltime %(hours)02i:%(minutes)02i \\
-      -oo out_%(base)s.out \\
-      -j oe \\
       -N %(base)s \\
-       'printenv; python %(python_script)s %(options)s %(filename)s'
+      -o out_%(base)s.out \\
+      -j oe \\
+      -q %(queue)s \\
+      -l walltime=%(hours)02i:%(minutes)02i \\
+      -l ttc=1 \\
+      '%(batch_script_name)s'
     """ % {
           "queue": queue,
           "hours": hours,
@@ -66,7 +83,10 @@ def main(python_script, filenames, options="", verbose=True):
           "python_script": python_script,
           "options": options,
           "filename": filename,
+          "batch_script_name":batch_script_name,
         }
+       #'printenv; python %(python_script)s %(options)s %(filename)s'
+
 
         if verbose:
             print script
