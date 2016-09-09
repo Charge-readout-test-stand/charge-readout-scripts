c55bef8acd51ddecd1ddc8fa1abc9363bac43b2e
alexis-s <alexis4@stanford.edu>
2016-09-09 16:48:15 -0700

    wip on new script

--- struck/reduce_root_file.py
+++ struck/reduce_root_file.py
@@ -1,13 +1,19 @@
 """
-Reduce tier3 file size by cutting most branches, events with too small
-SignalEnergy
+Reduce tier3 file size by cutting:
+  * most branches
+  * events with too small SignalEnergy
+
+https://root.cern.ch/root/html600/tutorials/tree/copytree2.C.html
+
 """
 
 import os
 import sys
 import ROOT
 
-# https://root.cern.ch/root/html600/tutorials/tree/copytree2.C.html
+import struck_analysis_cuts
+import struck_analysis_parameters
+
 
 
 if len(sys.argv) < 2:
@@ -19,15 +25,34 @@ filenames = sys.argv[1:]
 for filename in filenames:
 
     print "--> processing", filename
-    basename = os.path.basename(filename)
-    print basename
 
-    #continue # debugging
-
-
+    # open file, grab tree:
     tfile = ROOT.TFile(filename)
     oldtree = tfile.Get("tree")
     print "\t %i entries in old tree" % oldtree.GetEntries()
+    isMC = struck_analysis_parameters.is_tree_MC(oldtree)
+    print "\t isMC:", isMC
+
+    # construct some cuts...
+    selections = []
+    selections.append("(nsignals>0)")
+    #selections.append("(nsignals==1)")
+    selections.append("(SignalEnergy>100)")
+    selections.append(struck_analysis_cuts.get_drift_time_cut(isMC=isMC))
+    selections = "&&".join(selections)
+    print "selections:"
+    print selections[:150] # first ~line
+
+    # make new tree
+    basename = os.path.basename(filename)
+    newfile = ROOT.TFile("red_%s" % basename, "recreate")
+    newtree = oldtree.CloneTree(0) # the zero prepares branches but doesn't copy anything yet
+    newtree.SetTitle(selections)
+
+    # event list
+    oldtree.Draw(">>elist", selections)
+    elist = ROOT.gDirectory.Get("elist")
+    print "\t %i entries to copy" % elist.GetN()
 
     # turn on selected branches
     oldtree.SetBranchStatus("*",0) # first turn off all branches
@@ -37,24 +62,16 @@ for filename in filenames:
     oldtree.SetBranchStatus("nYsignals",1)
     oldtree.SetBranchStatus("energy1_pz",1)
     oldtree.SetBranchStatus("channel",1)
-    oldtree.SetBranchStatus("noise",1) # MC only 
     oldtree.SetBranchStatus("rise_time_stop95",1)
-
-    # new tree
-    newfile = ROOT.TFile("red_%s" % basename, "recreate")
-    newtree = oldtree.CloneTree(0) # the zero prepares branches but doesn't copy anything yet
-
-    # event list
-    oldtree.Draw(">>elist", "nsignals>0 && SignalEnergy>100")
-    elist = ROOT.gDirectory.Get("elist")
-    print "\t %i entries to copy" % elist.GetN()
-
-    # loop over event list
-    oldtree.SetEventList(elist)
+    if isMC:
+        oldtree.SetBranchStatus("noise",1) # MC only 
 
     reporting_percent = 5
     reporting_period = elist.GetN() * reporting_percent / 100
     print "\t reporting_period", reporting_period
+
+    # loop over event list
+    oldtree.SetEventList(elist)
     print "\t copying entries..."
     for i in xrange(elist.GetN()):
 
