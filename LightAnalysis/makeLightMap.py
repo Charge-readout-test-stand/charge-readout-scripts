83f4e72636a0b85bf99a90e97b706e33d5963691
alexis-s <alexis4@stanford.edu>
2016-12-09 15:22:44 -0800

    new light map bounds

--- LightAnalysis/makeLightMap.py
+++ LightAnalysis/makeLightMap.py
@@ -20,14 +20,14 @@ data = data_file.Get("data")
 
 # Ako's spacing is 0.2mm
 tfile = ROOT.TFile("SIPM_36_array_2016_12_09.root","recreate")
-hist = ROOT.TH2D("hist","Light detection probability, 36-SIPM array", 25, 0.0, 5.0, 25, 0.0, 5.0)
+hist = ROOT.TH2D("hist","Light detection probability, 36-SIPM array", 35, 0.0, 7.0, 15, 0.0, 3.0)
 hist.GetDirectory().cd()
 hist.SetXTitle("r [cm]")
 hist.SetYTitle("z [cm]")
 
 # Ako simulated multiple trials at each point, so weight by photon_nr/n_trials.
 n_drawn = data.Draw("zdir+%f:radius+%f >> hist" % (offset, offset),"photon_nr/%f" % n_trials,"goff")
-hist.SetMinimum(-0.001)
+hist.SetMinimum(0) # prevent 0 from showing as white
 
 canvas = ROOT.TCanvas("canvas","")
 canvas.SetRightMargin(0.15)
