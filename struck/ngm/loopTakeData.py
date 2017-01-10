5ed179027d86f4ecbde5bbb71b863b3e8aecc04e
lxereadout <lxe.readout@gmail.com>
2017-01-10 10:37:30 -0800

    increased loop length from 50 to 100 hours

--- struck/ngm/loopTakeData.py
+++ struck/ngm/loopTakeData.py
@@ -6,5 +6,5 @@ import time
 import takeData
 
 # take struck data in a loop!
-takeData.takeData(doLoop=True,n_hours=50.0) # set up the digitizer, take data
+takeData.takeData(doLoop=True,n_hours=100.0) # set up the digitizer, take data
 
