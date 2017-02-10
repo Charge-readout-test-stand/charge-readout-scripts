7e02b11739c3b12cee251cc8b2e6f44e863592f8
alexis-s <alexis4@stanford.edu>
2017-02-10 12:31:36 -0800

    mods for borax; compiling MC

--- llnl/setup_nEXO.sh
+++ llnl/setup_nEXO.sh
@@ -15,6 +15,19 @@ export LD_LIBRARY_PATH=/usr/tce/packages/gcc/gcc-4.9.3/lib64:$LD_LIBRARY_PATH
 export ROOTSYS=/usr/gapps/cern/root_v5.34.36/toss_3_x86_64/
 source $ROOTSYS/bin/thisroot.sh
 
+# needed for VGM:
+source /usr/gapps/cern/CLHEP.2311/setup
+export VGM_DIR=/g/g17/alexiss/software/vgm-borax/install/lib64/VGM-4.2.0
+
+# SET UP GEANT4v10 -- Feb 10 2017
+export G4INSTALL=/usr/gapps/cern/geant4.10.02.p02/  
+source $G4INSTALL/toss_3_x86_64-install/bin/geant4.sh
+export Geant4_DIR=$G4INSTALL/toss_3_x86_64-install/lib64/Geant4-10.2.2/ 
+export G4_V10=1
+
+# EXO-200 offline, needed for tier3 wfm processing with root 5:
+export EXOOUT=$HOME/software/offline/build
+source $EXOOUT/setup.sh
 
 #-------------------------------------------------------------------------------
 # old Aztec commands
