991f319017eacf11b22bbc5dd4bc528be9cd1a81
Alexis Schubert <alexis4@stanford.edu>
2017-02-22 09:29:40 -0800

    now printing wfm info, set line & marker color

--- nEXO_digitizer_study/draw_digi_waveforms.py
+++ nEXO_digitizer_study/draw_digi_waveforms.py
@@ -2,26 +2,48 @@
 
 import os
 import sys
-from ROOT import gROOT
-from ROOT import TTree
-from ROOT import TFile
-from ROOT import TCanvas
+import ROOT
 
+"""
+This script draws waveforms from nEXOdigi and prints info about them. 
+"""
 
 def main(filename):
   
-    digi_file = TFile(filename)
+    digi_file = ROOT.TFile(filename)
     tree = digi_file.Get("waveformTree")
     n_entries = tree.GetEntries()
-    canvas = TCanvas("canvas", "", 100, 20000)  
-    i_entry = 0
-    while i_entry < n_entries:
+    print "%i entries in tree" % n_entries
+
+    canvas = ROOT.TCanvas("canvas", "")  
+    canvas.SetGrid()
+
+    tree.SetLineColor(ROOT.kBlue)
+    tree.SetMarkerColor(ROOT.kBlue)
+    tree.SetMarkerSize(0.8)
+    tree.SetMarkerStyle(8)
+
+    for i_entry in xrange(n_entries):
+
         tree.GetEntry(i_entry)
-        i_entry+=1
+
+        print "entry %i | Evt %i | WFLen %i | WFTileId %i | WFLocalId %i | WFChannelCharge %i" % (
+            i_entry, 
+            tree.EventNumber,
+            tree.WFLen,
+            tree.WFTileId,
+            tree.WFLocalId,
+            tree.WFChannelCharge,
+        )
+
         selection = "Entry$==%i" % i_entry
-        tree.Draw("WFAmplitude:WFTime",selection,"l")
+        tree.Draw("WFAmplitude:WFTime",selection,"pl")
         canvas.Update()
-        raw_input("press enter") 
+
+        val = raw_input("press enter (q to quit) ") 
+        if val == 'q':
+            sys.exit()
+
 
 if __name__ == "__main__":
 
