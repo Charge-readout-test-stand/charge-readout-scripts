bb2fb1eae11ee91200f8a3c1e6097eba5eb276d3
lxereadout <lxe.readout@gmail.com>
2017-04-04 10:56:50 -0700

    updates from sipm trigger testing

--- struck/drawMaw.py
+++ struck/drawMaw.py
@@ -36,6 +36,19 @@ gStyle.SetTitleBorderSize(0)
 
 def process_file(filename):
 
+    # options
+    channel = 0
+
+    # maw options
+    #gap_time = 30 # delay
+    #peaking_time = 10 # length of moving average unit
+
+    # gt 30, pt 10 seems ok for 250 MS/s
+    # gt 30, pt 2 to 4 seems ok for 25 MS/s
+    gap_time = 30 # delay
+    peaking_time = 2 # length of moving average unit
+
+
     print "processing file: ", filename
 
     basename = os.path.basename(filename)
@@ -43,7 +56,7 @@ def process_file(filename):
 
     # open the root file and grab the tree
     root_file = TFile(filename)
-    tree = root_file.Get("tree9")
+    tree = root_file.Get("tree%i" % channel)
     n_entries = tree.GetEntries()
     print "%i entries" % n_entries
 
@@ -51,28 +64,22 @@ def process_file(filename):
 
     # set up a canvas
     canvas = TCanvas("canvas","", 700, 900)
-    canvas.Divide(1,2)
+    canvas.Divide(1,3)
 
-
-    #maw_graph = TGraph()
-
-    peaking_time = 4
-    gap_time = 10
 
     i_entry = 0
     while i_entry < n_entries:
 
         tree.GetEntry(i_entry)
 
-        #for i in xrange(tree.sample_length):
-        #    print i_entry, i, tree.wfm[i], tree.maw[i]
-        #    maw_graph.SetPoint(i, tree.maw[i])
-        #graph.Draw("al")
-        channel = ord(tree.channel)
+        try:
+            channel = ord(tree.channel) # old versions
+        except TypeError:
+            channel = tree.channel
         print "entry %i, channel %i" % (i_entry, channel)
-        if channel != 9:
-            i_entry+=1
-            continue
+        #if channel != 9:
+        #    i_entry+=1
+        #    continue
 
         tree.SetLineColor(TColor.kBlue+1)
 
@@ -85,13 +92,52 @@ def process_file(filename):
         legend = TLegend(0.7, 0.92, 0.9, 0.99)
         legend.AddEntry(tree, "channel %i" % channel, "pl")
         selection = "Entry$==%i" % i_entry
-        selection += " && Iteration$>0 && Iteration$<300"
+        #selection += " && Iteration$>165 && Iteration$<250"
+        selection += " && Iteration$>120 && Iteration$<500"
         tree.Draw("wfm:Iteration$",selection, "lp")
         legend.Draw()
 
+        # try to reconstruct the MAW:
+        wfm = tree.wfm
+        maw_graph = TGraph()
+        maw_graph.SetLineWidth(2)
+        maw_graph.SetLineColor(TColor.kRed)
+        maw_graph.SetMarkerStyle(8)
+        maw_graph.SetMarkerSize(0.8)
+        ma1 = 0
+        ma2 = 0 # delayed
+        for i in xrange(tree.wfm_length):
+
+            # construct ma1, the first "moving average sum"
+            ma1 += wfm[i]
+            j = i - peaking_time # remove old part of ma1
+            if j < 0: j = 0
+            ma1 -= wfm[j]
+
+            # construct ma2, the 2nd "moving average sum"
+            j = i - peaking_time - gap_time
+            if j < 0: j = 0
+            ma2 += wfm[j]
+            j = i - peaking_time - gap_time - peaking_time
+            if j < 0: j = 0
+            ma2 -= wfm[j]
+
+            maw_val = ma1 - ma2
+            maw_graph.SetPoint(i, i, maw_val)
+            #print "%i : ma1: %i | ma2: %i" % (i, ma1, ma2)
+            # done building MAW
+
+
         pad = canvas.cd(2)
         pad.SetGrid(1,1)
-        tree.Draw("maw:Iteration$","Entry$==%i" % i_entry, "l")
+        #tree.Draw("maw:Iteration$","Entry$==%i" % i_entry, "l")
+        tree.Draw("maw:Iteration$",selection, "lp")
+        maw_graph.Draw("l same")
+
+
+        pad = canvas.cd(3)
+        pad.SetGrid(1,1)
+        maw_graph.Draw("al")
 
         canvas.Update()
         val = raw_input("--> entry %i | ch %i | enter to continue (q to quit, p to print, or entry number) " % (i_entry, channel))
