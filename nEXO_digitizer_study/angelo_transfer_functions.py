f52b741c4212428e31c8348659b344bfc9ff8d1f
lxereadout <lxe.readout@gmail.com>
2017-04-13 19:31:02 -0700

    working on current wfm

--- nEXO_digitizer_study/angelo_transfer_functions.py
+++ nEXO_digitizer_study/angelo_transfer_functions.py
@@ -21,7 +21,9 @@ bandwidth so that you can compare directly the two functions.
 
 import numpy as np
 from scipy.fftpack import fft
-from digi import RalphWF
+import matplotlib.pyplot as plt
+from mc.digi import RalphWF
+from mc import digi
 
 def simple_gaus(s, tau):
     return 10.954/(0.83*tau*s+1.477)/(pow(0.83* tau*s+1.417, 2.0)+pow(0.598,2))/(pow(0.83* tau*s+1.204, 2)+pow(1.299,2))
@@ -33,27 +35,61 @@ def bessel(s, tau):
 tau = 1.0
 s = 0
 
+# physical constants
+cathodeToAnodeDistance = 1e3 # mm
+drift_velocity = 1.71 # mm/microsecond
 
+# PCD coords
+pcdx= 1.5 # mm
+pcdy = 0.0
+pcdz = 0.0 # at cathode
+pcdz = cathodeToAnodeDistance - 50.0 # mm, distance from cathode
+
+# options
+sample_freq_MHz = 2.0
+sampling_period = 10.0/(sample_freq_MHz) # microsecond 
+#dZ = cathodeToAnodeDistance/200.0
+dZ = drift_velocity * sampling_period 
+#wfm_length = int(cathodeToAnodeDistance/dZ)+1 # samples
+wfm_length = int((cathodeToAnodeDistance-pcdz)/dZ)+10 # samples
+sampling_period = dZ/drift_velocity
 
 print "simple_gaus(%s, %s): %f" % (s, tau, simple_gaus(s, tau))
 print "bessel(%s, %s): %f" % (s, tau, bessel(s, tau))
 
 
-# a sample waveform:
-pcdx= 1.5
-pcdy = 0.0
-pcdz = 0.0
+# a sample waveform, using Ralph's analytical method:
+ralph_WF = digi.RalphWF.make_WF(xpcd=pcdx, ypcd=pcdy, zpcd=pcdz, Epcd=1, chID=15,
+        cathodeToAnodeDistance=cathodeToAnodeDistance, dZ=dZ, wfm_length=wfm_length)
 
-sample_times = np.arange(800)*40*1e-3
+# sampling time points
+sample_times = np.arange(wfm_length)*sampling_period
+
 plt.figure(1)
 #Collection signal on channel X16
 plt.title("Collection signal X16")
 plt.xlabel("Time[$\mu$s]")
 plt.ylabel("Q/Qtotal")
-ralph_WF = RalphWF.make_WF(xpcd=pcdx, ypcd=pcdy, zpcd=pcdz, Epcd=1, chID=15)
+plt.grid(b=True)
 plt.plot(sample_times, ralph_WF)
-plt.ylim([-np.max(WF)*0.1, np.max(WF)*1.1])
+plt.ylim([-np.max(ralph_WF)*0.1, np.max(ralph_WF)*1.1])
 #plt.savefig("collect_X16.png")
+raw_input("press enter to continue ")
+
+current_WF = digi.RalphWF.make_current_WF(xpcd=pcdx, ypcd=pcdy, zpcd=pcdz,
+        Epcd=1, chID=15, cathodeToAnodeDistance=cathodeToAnodeDistance, dZ=dZ,
+        wfm_length=wfm_length)
+
+plt.figure(2)
+#Collection signal on channel X16
+plt.title("Current signal X16")
+plt.xlabel("Time[$\mu$s]")
+plt.ylabel("I/Itotal")
+plt.grid(b=True)
+plt.plot(sample_times, current_WF)
+plt.ylim([-np.max(current_WF)*0.1, np.max(current_WF)*1.1])
+#plt.savefig("collect_X16.png")
+raw_input("press enter to continue ")
 
 
 
