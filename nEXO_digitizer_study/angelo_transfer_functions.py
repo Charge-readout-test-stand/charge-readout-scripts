7bdd9707a1339857e377eb11c805dc86f7e66f5d
lxereadout <lxe.readout@gmail.com>
2017-04-17 11:08:22 -0700

    now drawing current wfm calc from charge wfm derivative

--- nEXO_digitizer_study/angelo_transfer_functions.py
+++ nEXO_digitizer_study/angelo_transfer_functions.py
@@ -46,12 +46,13 @@ pcdz = 0.0 # at cathode
 pcdz = cathodeToAnodeDistance - 50.0 # mm, distance from cathode
 
 # options
-sample_freq_MHz = 2.0
-sampling_period = 10.0/(sample_freq_MHz) # microsecond 
+sample_freq_MHz = 2.0 # nEXO digitizer
+multiplier = 10.0 # oversample
+sampling_period = 1.0/(sample_freq_MHz*multiplier) # microsecond (oversampled)
 #dZ = cathodeToAnodeDistance/200.0
 dZ = drift_velocity * sampling_period 
 #wfm_length = int(cathodeToAnodeDistance/dZ)+1 # samples
-wfm_length = int((cathodeToAnodeDistance-pcdz)/dZ)+10 # samples
+wfm_length = int((cathodeToAnodeDistance-pcdz)/dZ)+20 # drift_time / sampling_period samples
 sampling_period = dZ/drift_velocity
 
 print "simple_gaus(%s, %s): %f" % (s, tau, simple_gaus(s, tau))
@@ -62,6 +63,8 @@ print "bessel(%s, %s): %f" % (s, tau, bessel(s, tau))
 ralph_WF = digi.RalphWF.make_WF(xpcd=pcdx, ypcd=pcdy, zpcd=pcdz, Epcd=1, chID=15,
         cathodeToAnodeDistance=cathodeToAnodeDistance, dZ=dZ, wfm_length=wfm_length)
 
+print "length of ralph_WF: ", len(ralph_WF)
+
 # sampling time points
 sample_times = np.arange(wfm_length)*sampling_period
 
@@ -71,22 +74,34 @@ plt.title("Collection signal X16")
 plt.xlabel("Time[$\mu$s]")
 plt.ylabel("Q/Qtotal")
 plt.grid(b=True)
-plt.plot(sample_times, ralph_WF)
+plt.plot(sample_times, ralph_WF, 'o-')
 plt.ylim([-np.max(ralph_WF)*0.1, np.max(ralph_WF)*1.1])
 #plt.savefig("collect_X16.png")
-raw_input("press enter to continue ")
+#raw_input("press enter to continue ")
 
 current_WF = digi.RalphWF.make_current_WF(xpcd=pcdx, ypcd=pcdy, zpcd=pcdz,
         Epcd=1, chID=15, cathodeToAnodeDistance=cathodeToAnodeDistance, dZ=dZ,
         wfm_length=wfm_length)
+print "length of current_WF: ", len(current_WF)
+
+current_WF_deriv = digi.RalphWF.make_current_from_derivative(xpcd=pcdx,
+        ypcd=pcdy, zpcd=pcdz, Epcd=1, chID=15,
+        cathodeToAnodeDistance=cathodeToAnodeDistance, dZ=dZ,
+        wfm_length=wfm_length)
+print "length of current_WF_deriv: ", len(current_WF_deriv)
+
+
+
 
 plt.figure(2)
 #Collection signal on channel X16
 plt.title("Current signal X16")
 plt.xlabel("Time[$\mu$s]")
-plt.ylabel("I/Itotal")
+plt.ylabel("I / Q_total")
 plt.grid(b=True)
-plt.plot(sample_times, current_WF)
+#plt.semilogy()
+plt.plot(sample_times, current_WF,'.-')
+plt.plot(sample_times[:-1], current_WF_deriv,'.-')
 plt.ylim([-np.max(current_WF)*0.1, np.max(current_WF)*1.1])
 #plt.savefig("collect_X16.png")
 raw_input("press enter to continue ")
