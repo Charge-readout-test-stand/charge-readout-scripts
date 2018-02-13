2b0d691e3fe8b361f527a87acb9b600717f8c6f3
lxereadout <lxe.readout@gmail.com>
2018-02-13 15:04:56 -0800

    Updates for analysis of 13th LXe data

--- struck/redoCalibration.py
+++ struck/redoCalibration.py
@@ -3,30 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os,sys
 import struck_analysis_parameters
+import struck_analysis_cuts_sipms
 import scipy.optimize as opt
+import FitPeakPy
 plt.ion()
 plt.figure(1)
-
-def get_cut(ch):
-    selection = []
-    selection.append("nfound_channels==30") #cut the dead channel events 
-    selection.append("SignalEnergy > 100")
-    selection.append("nsignals==1")
-    selection.append("(nXsignals==1 || nYsignals==1)")
-    selection.append("channel==%i" % ch)
-    selection.append("(rise_time_stop95_sum-trigger_time) > 10")
-    selection.append("(rise_time_stop95_sum-trigger_time) < 15")
-    selection = " && ".join(selection)
-    return selection
-
-def get_cut_full():
-    selection = []
-    selection.append("nfound_channels==30") #cut the dead channel events 
-    selection.append("SignalEnergy > 100")
-    selection.append("nsignals==2")
-    selection.append("(nXsignals==1 && nYsignals==1)")
-    selection = " && ".join(selection)
-    return selection
 
 def get_dcmd_full():
     draw_array = []
@@ -43,75 +24,10 @@ def get_dcmd(ch):
     draw_cmd =  ":".join(draw_array)
     return draw_cmd, len(draw_array)
 
-def ffn(x, A1, mu, sig, E1, E2, E3):
-    gaus = A1*np.exp(-(x-mu)**2/(2*sig**2))
-    exp  = E1*np.exp(-E2*x) + E3 
-    return gaus + exp
-
-def ffn_sep(x, A1, mu, sig, E1, E2, E3):
-    gaus = A1*np.exp(-(x-mu)**2/(2*sig**2))
-    exp  = E1*np.exp(-E2*x) + E3
-    return gaus , exp
-
-def FitPeak(hist_counts, hist_errors, bin_centers):
-    de = 200 # Fit width
-    peak_pos = 570.0 #bin_centers[np.argmax(hist_counts)]
-    
-    print "Peak Guess", peak_pos
-
-    #Only fit the bins within a range de of the peak
-    fit_min = peak_pos-de
-    fit_max = peak_pos+de
-    fpts = np.logical_and( bin_centers > peak_pos-de, bin_centers < peak_pos+de )
-
-    print (hist_counts[fpts])[0], (hist_counts[fpts])[-1], fit_min, fit_max
-    start_height = (hist_counts[fpts])[0]
-    end_height   = (hist_counts[fpts])[-1]
-    if end_height == 0: end_height = 1e-16
-    if start_height == 0: start_height = 1.0
-    #exp_decay_guess  = np.log(fit_max/fit_min)/(2*de)
-    exp_decay_guess   = np.log(1.0*start_height/end_height)*(1/(2.0*de))
-    exp_height_guess = (hist_counts[fpts])[0]*np.exp(fit_min*exp_decay_guess) 
-    sigma_guess  = 35.0
-    height_guess = hist_counts[np.argmin(np.abs(bin_centers - peak_pos))] - exp_height_guess*np.exp(-exp_decay_guess*peak_pos)
-    spars = [height_guess, peak_pos, sigma_guess, exp_height_guess, exp_decay_guess, 0.] #Initial guess for fitter
-    
-    print "Height Guess:", height_guess
-    print "Sigma  Guess:", sigma_guess
-    print "Peak Pos:", peak_pos
-    print "Exp Height Guess:", exp_height_guess
-    print "EXP Decay Guess", exp_decay_guess
-    print abs((hist_counts[fpts])[0] - (hist_counts[fpts])[-1]), (2*de)
-    print -1*np.log(abs((hist_counts[fpts])[0] - (hist_counts[fpts])[-1])/(2.0*de))
-
-    fail = False
-
-    #Perform the fit
-    try:
-        bp, bcov = opt.curve_fit(ffn, bin_centers[fpts], hist_counts[fpts], p0=spars, sigma=hist_errors[fpts])
-    except RuntimeError:
-        print "**********************Did not work*******************************"
-        fail = True
-        bp = spars
-        bcov = np.eye(len(bp))
-    
-    #Get the x values for evaluating the function
-    xx = np.linspace( bin_centers[fpts][0], bin_centers[fpts][-1], 1e3 )
-    
-    print ""
-    print "-------Fit Results----------"
-    print "Mean:  %.2f" % bp[1]
-    print "Sigma: %.2f" % bp[2]
-    print "EXP Decay Final", bp[4]
-    print "----------------------------"
-    print
-
-    return xx, bp, bcov, fail
-
-
-def process_file(filename):
-    tfile = ROOT.TFile(filename)
-    tree = tfile.Get("tree")
+def process_file(file_list):
+    tree = ROOT.TChain("tree")
+    for filename in file_list:
+        tree.Add(filename)
     tree.SetEstimate(tree.GetEntries())
 
     #Speed things up by setting on and off branches
@@ -143,7 +59,8 @@ def process_file(filename):
         if ch==0:
             cal_file.write("calibration_values[%i] = %f # +/- None %s\n" % (i, calibration[i], channel_map[i]))
             continue
-        selectcmd = get_cut(i)
+        
+        selectcmd = struck_analysis_cuts_sipms.get_chcal_cut(ch)
         drawcmd,nvals   = get_dcmd(i)
         print "Draw CMD",  drawcmd
         print "Select CMD",selectcmd
@@ -159,14 +76,12 @@ def process_file(filename):
 
         bin_centers = bin_edges[:-1] + np.diff(bin_edges)/2.0
      
-        fitx, fitp, fit_cov, isFail = FitPeak(chHist, np.sqrt(chHist), bin_centers)
+        fitx, fitp, fit_cov, isFail, full_fit, gaus, exp = FitPeakPy.FitPeak(chHist, np.sqrt(chHist), bin_centers)
 
         plt.title("Channel %s" % channel_map[i], fontsize=14)
         plt.xlabel("Energy[keV]", fontsize=15)
         plt.ylabel("Counts/%.2f keV" % np.diff(bin_edges)[0], fontsize=15)
         plt.errorbar(bin_centers, chHist, yerr=np.sqrt(chHist),marker='o', linestyle='None', c='k')
-        gaus,exp = ffn_sep(fitx, fitp[0], fitp[1], fitp[2], fitp[3], fitp[4], fitp[5])
-        full_fit = ffn(fitx, fitp[0], fitp[1], fitp[2], fitp[3], fitp[4], fitp[5])
         
         print "New Calibration for %s: %.4f" % (channel_map[i], (570./fitp[1])*calibration[i])
         
@@ -182,7 +97,7 @@ def process_file(filename):
             #Failed the fit
             cal_file.write("calibration_values[%i] = %f # +/- %f %s \n" % (i, calibration[i], 0.0, channel_map[i]))
 
-        plt.savefig("./plots/calibration_fit_ch%s.pdf" % channel_map[i])
+        plt.savefig("./plots/cal/calibration_fit_ch%s.pdf" % channel_map[i])
         
         #if channel_map[i]=="Y14": raw_input()
         raw_input()
@@ -190,40 +105,40 @@ def process_file(filename):
 
     #Now do for the Signal Energy
     drawcmd,nvals   = get_dcmd_full()
-    selectcmd = get_cut_full()
+    selectcmd = struck_analysis_cuts_sipms.get_fullcal_cut()
     tree.Draw(drawcmd,selectcmd,"goff")
     n = tree.GetSelectedRows()
     channelEnergy =  np.array([tree.GetVal(0)[j] for j in xrange(n)])
     chHist,bin_edges = np.histogram(channelEnergy, bins=120, range=(200,1200))
     bin_centers = bin_edges[:-1] + np.diff(bin_edges)/2.0
-    fitx, fitp, fit_cov, isFail = FitPeak(chHist, np.sqrt(chHist), bin_centers)
+    fitx, fitp, fit_cov, isFail, full_fit, gaus, exp = FitPeakPy.FitPeak(chHist, np.sqrt(chHist), bin_centers)
     plt.title("Signal Energy", fontsize=14)
     plt.xlabel("Energy[keV]", fontsize=15)
     plt.ylabel("Counts/%.2f keV" % np.diff(bin_edges)[0], fontsize=15)
-
-    full_fit = ffn(fitx, fitp[0], fitp[1], fitp[2], fitp[3], fitp[4], fitp[5])
     plt.errorbar(bin_centers, chHist, yerr=np.sqrt(chHist),marker='o', linestyle='None', c='k')
     plt.plot(fitx, full_fit, c='r')
     res = (fitp[2]/fitp[1])*100
-    gaus,exp = ffn_sep(fitx, fitp[0], fitp[1], fitp[2], fitp[3], fitp[4], fitp[5])
     plt.plot(fitx, gaus, c='b', label="%.2f %% \n %.2f keV" % (res, fitp[1]))
     plt.plot(fitx, exp, c='g')
     plt.legend()
-    plt.savefig("./plots/full_fit_SignalEnergy.pdf")
+    plt.savefig("./plots/cal/full_fit_SignalEnergy.pdf")
     raw_input()
 
 if __name__ == "__main__":
 
-    filename = None
+    file_list = None
     if len(sys.argv) < 2:
         print "argument: [sis tier 3 root file]"
         #sys.exit(1)
         #filename = "overnight_new_bias_tier3_all_v1_12_3_2017.root"
         #filename = "overnight_new_bias_tier3_all_v2_12_4_2017.root"
-        filename  = "overnight_new_bias_tier3_all_v3_12_6_2017.root"
+        #filename  = "overnight_new_bias_tier3_all_v3_12_6_2017.root"
+        file_list   = ["all_tier3_overnight_day2_315bias.root",
+                       "~/2018_01_26_SiPM_Run2/full_cell_day_runs/tier3_added/all_tier3_day1_305bias.root",
+                       "~/2018_01_26_SiPM_Run2/full_cell_day_runs_2/tier3_added/all_tier3_day2_305bias.root"]
     else:
-        filename = sys.argv[1]
+        file_list = sys.argv[1:]
 
-    print "Using ", filename
-    process_file(filename)
+    print "Using ", file_list
+    process_file(file_list)
 
